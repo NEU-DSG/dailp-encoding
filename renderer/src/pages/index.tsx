0ed02a347e7c92b8670260a12d27e93e6bfc82a6
Taylor Snead <taylorsnead@gmail.com>
2020-07-17 10:02:34 -0400

    :art: Pull AnnotatedElement into its own file

--- renderer/src/pages/index.tsx
+++ renderer/src/pages/index.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { graphql } from "gatsby"
 import xml from "xml-parser"
 import { Helmet } from "react-helmet"
+import { AnnotatedElement } from "../annotated-element"
 
 export default ({ data }) => {
   // Parse data from story.xml
@@ -20,58 +21,17 @@ export default ({ data }) => {
 
 const AnnotatedSource = (props: { body: any }) => (
   <div className="full_source">
-    {props.body.children.map((elem: any, idx) => (
+    {props.body.children.map((elem: any, idx: number) => (
       <AnnotatedElement key={idx} element={elem} />
     ))}
   </div>
 )
 
-const PageBreak = () => (
-  <div style={{ margin: 50 }}>Page Start</div>
-)
-
-const AnnotatedElement = (props: { element: any }) => {
-  switch (props.element.name) {
-    case "ab":
-      return (
-        <div style={{ display: "flex", flexFlow: "row wrap" }}>
-          {props.element.children.map(e => (
-            <AnnotatedElement key={e.attributes["xml:id"]} element={e} />
-          ))}
-        </div>
-      )
-    case "phr":
-    case "seg":
-      return props.element.children.map(e => (
-        <AnnotatedElement key={e.attributes["xml:id"]} element={e} />
-      ))
-    case "pb":
-      return <PageBreak />
-    case "w":
-      return <AnnotatedWord elem={props.element} />
-    case "lb":
-      return <div style={{ flexBasis: "100%" }} />
-    default:
-      return null
-  }
-}
-
-const AnnotatedWord = (props: { elem: any }) => {
-  const choice = props.elem.children[0]
-  const layers = choice.children
-  return <div style={{ marginBottom: 50, marginRight: 50 }}>
-    {layers.map(layer => (<div>{layer.content}</div>))}
-  </div>
-}
-
+// Pull our XML from any data source.
 export const query = graphql`
   query {
     allFile(filter: { name: { eq: "story" } }) {
-      nodes {
-        internal {
-          content
-        }
-      }
+        nodes { internal { content } }
     }
   }
 `
