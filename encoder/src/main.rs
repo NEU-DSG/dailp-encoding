b4a65d9b995c4b2ba898e31142eaed769e4dd5e5
Taylor Snead <taylorsnead@gmail.com>
2020-08-07 19:34:36 -0400

    :sparkles: Parse the index sheet for a document list

--- encoder/src/main.rs
+++ encoder/src/main.rs
@@ -3,25 +3,32 @@ mod retrieve;
 mod translation;
 
 pub const GOOGLE_API_KEY: &str = "AIzaSyBqqPrkht_OeYUSNkSf_sc6UzNaFhzOVNI";
-pub const ANNOTATED_SHEETS: &[&str] = &[
-    "188qlHBCMLSpuo9A1J5KIiCQv3iCuKfBpt9T0XDU_PH4",
-    "1oUF0ajzc46TEgfkgh4cHBnlQovW_hCk9pgnGh3dZ-Es",
-    "1x5wamVrV5W1g3rrCL5TrOkyKx0m4-d7FIlLrIbG_R2I",
-];
 
 use anyhow::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    for sheet_id in ANNOTATED_SHEETS {
-        let sheet = retrieve::SheetResult::from_sheet(sheet_id, None)
+    // Pull the list of annotated documents from our master index sheet.
+    let index =
+        retrieve::SheetResult::from_sheet("1sDTRFoJylUqsZlxU57k1Uj8oHhbM3MAzU8sDgTfO7Mk", None)
+            .await?
+            .into_index()?;
+
+    for sheet_id in index.sheet_ids {
+        // Split the contents of each main sheet into semantic lines with
+        // several layers.
+        let sheet = retrieve::SheetResult::from_sheet(&sheet_id, None)
             .await?
             .split_into_lines();
-        let meta = retrieve::SheetResult::from_sheet(sheet_id, Some("Metadata"))
+
+        // Parse the metdata on the second page of each sheet.
+        // This includes publication information and a link to the translation.
+        let meta = retrieve::SheetResult::from_sheet(&sheet_id, Some("Metadata"))
             .await?
             .into_metadata()
             .await?;
 
+        // Pass all of this into the TEI output.
         encode::write_to_file(meta, sheet)?;
     }
     Ok(())
