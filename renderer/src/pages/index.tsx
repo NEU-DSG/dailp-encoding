4fde54f2befaca037eb26427686895976865e9ee
Taylor Snead <taylorsnead@gmail.com>
2020-08-25 09:18:33 -0400

    :lipstick: Upgrade the react setup with graphql

--- renderer/src/pages/index.tsx
+++ renderer/src/pages/index.tsx
@@ -1,37 +1,39 @@
 import React from "react"
-import { graphql } from "gatsby"
-import xml from "xml-parser"
+import { graphql, Link } from "gatsby"
 import { Helmet } from "react-helmet"
-import { AnnotatedElement } from "../annotated-element"
+import Layout from "../layout"
 
 export default ({ data }) => {
-  // Parse data from story.xml
-  const story = xml(data.allFile.nodes[0].internal.content)
-  // Find the annotated text within.
-  const texts = story.root.children.find(t => t.name == "text").children[0]
-  const annotation = texts.children.find(t => t.attributes.type == "source")
-
-  return <>
-    <Helmet>
-      <title>DAILP Document Viewer</title>
-    </Helmet>
-    <AnnotatedSource body={annotation.children[0]} />
-  </>
+  const documents = data.dailp.documents
+  return (
+    <Layout>
+      <Helmet>
+        <title>DAILP Document Viewer</title>
+      </Helmet>
+      <main>
+        <ul>
+          {documents.map((document: any) => (
+            <li key={document.id}>
+              <Link to={`/documents/${document.id.toLowerCase()}`}>
+                {document.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </Layout>
+  )
 }
-
-const AnnotatedSource = (props: { body: any }) => (
-  <div className="full_source">
-    {props.body.children.map((elem: any, idx: number) => (
-      <AnnotatedElement key={idx} element={elem} />
-    ))}
-  </div>
-)
 
 // Pull our XML from any data source.
 export const query = graphql`
   query {
-    allFile(filter: { name: { eq: "story" } }) {
-        nodes { internal { content } }
+    dailp {
+      documents {
+        id
+        title
+        source
+      }
     }
   }
 `
