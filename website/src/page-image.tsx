d85e8b60198794be0883088100a762f9659c4236
Taylor Snead <taylor@snead.xyz>
2021-01-06 11:58:06 -0500

    Add manual zoom controls to document images
    Add buttons that allow zooming in and out of document images on the
    desktop site.

--- website/src/page-image.tsx
+++ website/src/page-image.tsx
@@ -1,9 +1,11 @@
-import React, { useRef, useState } from "react"
+import React, { useState } from "react"
 import { css, cx } from "linaria"
 import theme, { hideOnPrint, std, typography } from "./theme"
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch"
 import { Helmet } from "react-helmet"
 import { Button } from "reakit/Button"
+import { MdZoomIn, MdZoomOut } from "react-icons/md"
+import { FaMinus, FaPlus } from "react-icons/fa"
 
 const PageImages = (p: {
   document: GatsbyTypes.Dailp_AnnotatedDoc
@@ -20,21 +22,23 @@ const PageImages = (p: {
       </Helmet>
       <TransformWrapper
         defaultScale={1}
-        defaultPositionX={0}
-        defaultPositionY={0}
+        positionX={0}
+        positionY={0}
         wheel={{ disabled: true }}
         options={{ maxScale: 6, centerContent: true }}
       >
-        {({ setTransform }) => (
+        {({ setTransform, zoomIn, zoomOut }) => (
           <CurrentPageImage
             resetTransform={() => setTransform(0, 0, 1, 200, "easeOut")}
+            zoomIn={zoomIn}
+            zoomOut={zoomOut}
             pageImages={p.pageImages}
           />
         )}
       </TransformWrapper>
       {p.document.sources.length ? (
         <figcaption className={caption}>
-          Courtesy of{" "}
+          Source:{" "}
           <a href={p.document.sources[0].link}>{p.document.sources[0].name}</a>
         </figcaption>
       ) : null}
@@ -44,7 +48,9 @@ const PageImages = (p: {
 export default PageImages
 
 const CurrentPageImage = (p: {
-  resetTransform: any
+  resetTransform: () => void
+  zoomIn: () => void
+  zoomOut: () => void
   pageImages: readonly string[]
 }) => {
   const [selectedPage, setSelectedPage] = useState(0)
@@ -64,7 +70,9 @@ const CurrentPageImage = (p: {
           >
             Previous
           </Button>
+
           <span aria-current="true">Page {selectedPage + 1}</span>
+
           <Button
             className={std.button}
             onClick={() => {
@@ -78,16 +86,44 @@ const CurrentPageImage = (p: {
           </Button>
         </nav>
       )}
-      <TransformComponent>
-        <img
-          className={pageImage}
-          src={url}
-          alt={`Manuscript Page ${selectedPage + 1}`}
-        />
-      </TransformComponent>
+      <div style={{ position: "relative" }}>
+        <TransformComponent>
+          <img
+            className={pageImage}
+            src={url}
+            alt={`Manuscript Page ${selectedPage + 1}`}
+          />
+        </TransformComponent>
+        <div className={floatingControls}>
+          <Button
+            onClick={p.zoomIn}
+            className={std.iconButton}
+            aria-label="Zoom In"
+          >
+            <FaPlus size={20} />
+          </Button>
+          <Button
+            onClick={p.zoomOut}
+            className={std.iconButton}
+            aria-label="Zoom Out"
+          >
+            <FaMinus size={20} />
+          </Button>
+        </div>
+      </div>
     </>
   )
 }
+
+const floatingControls = css`
+  position: absolute;
+  right: 0.5rem;
+  bottom: 0.5rem;
+  background: ${theme.colors.body};
+  & > * {
+    display: block;
+  }
+`
 
 const pageNav = css`
   display: flex;
@@ -110,8 +146,9 @@ const caption = css`
 const annotationFigure = css`
   width: 100%;
   margin: 0;
-  margin-bottom: ${typography.rhythm(2)}rem;
+  margin-bottom: ${typography.rhythm(2)};
   .react-transform-component {
+    position: relative;
     cursor: move;
     cursor: grab;
     max-height: 20rem;
