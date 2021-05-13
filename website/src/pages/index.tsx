cf443ed70fd509cb24d1914c46758b92a38dae14
Taylor Snead <taylor@snead.xyz>
2021-05-12 23:25:03 -0400

    Add React carousel component
    Replace the broken WordPress carousel with a React component.

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -3,11 +3,12 @@ import { graphql, Link } from "gatsby"
 import { styled } from "linaria/react"
 import { css, cx } from "linaria"
 import { Helmet } from "react-helmet"
+import "@fortawesome/fontawesome-free/css/fontawesome.css"
+import "@fortawesome/fontawesome-free/css/solid.css"
 import Layout from "../layout"
 import theme, { fullWidth, wordpressUrl } from "../theme"
 import { collectionRoute } from "../routes"
-import "@fortawesome/fontawesome-free/css/fontawesome.css"
-import "@fortawesome/fontawesome-free/css/solid.css"
+import { Carousel } from "../carousel"
 
 /** Lists all documents in our database */
 const IndexPage = (props: { data: GatsbyTypes.IndexPageQuery }) => (
@@ -18,31 +19,13 @@ const IndexPage = (props: { data: GatsbyTypes.IndexPageQuery }) => (
         rel="stylesheet"
         href={`${wordpressUrl}/wp-content/themes/quest-child/style.css?ver=4.9.16`}
       />
-      <script
-        type="text/javascript"
-        src="https://code.jquery.com/jquery-1.12.4.min.js"
-        integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
-        crossOrigin="anonymous"
-      ></script>
-      <script
-        type="text/javascript"
-        src={`${wordpressUrl}/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1`}
-      ></script>
-      <script
-        type="text/javascript"
-        src={`${wordpressUrl}/wp-content/themes/quest/assets/js/quest-and-plugins.js?ver=4.9.16`}
-      ></script>
-      <script
-        type="text/javascript"
-        src={`${wordpressUrl}/wp-content/plugins/drs-tk/assets/js/gallery.js?ver=4.9.16`}
-      ></script>
-      <script
-        type="text/javascript"
-        src={`${wordpressUrl}/wp-includes/js/imagesloaded.min.js?ver=3.2.0`}
-      ></script>
     </Helmet>
     <DocIndex>
       <FullWidth>
+        <Carousel
+          images={carouselImages}
+          caption="Digital Archive of American Indian Languages Preservation and Perseverance"
+        />
         <div
           dangerouslySetInnerHTML={{ __html: props.data.aboutPage?.content }}
         />
@@ -77,70 +60,17 @@ export const query = graphql`
   }
 `
 
+const carouselImages = [
+  "https://wp.dailp.northeastern.edu/wp-content/uploads/2020/01/Screen-Shot-2020-01-07-at-7.53.23-PM-300x253.png",
+  "https://wp.dailp.northeastern.edu/wp-content/uploads/2020/01/Screen-Shot-2020-01-07-at-9.30.13-PM-184x300.png",
+]
+
 export const DocIndex = styled.main`
   padding-left: ${theme.edgeSpacing};
   padding-right: ${theme.edgeSpacing};
   display: flex;
   flex-flow: column wrap;
   align-items: center;
-
-  .carousel {
-    display: none;
-    position: relative;
-    .carousel-inner {
-      position: relative;
-      overflow: hidden;
-    }
-    .carousel-control {
-      position: absolute;
-      top: 0;
-      bottom: 0;
-      width: 15%;
-      font-size: 20px;
-      color: ${theme.colors.text};
-      text-align: center;
-      & > * {
-        position: absolute;
-        top: 50%;
-      }
-      &.right {
-        right: 0;
-        left: auto;
-      }
-    }
-    .item {
-      position: relative;
-      transition: transform 0.6s ease-in-out;
-      backface-visibility: hidden;
-      display: none;
-      left: 0;
-
-      &.active {
-        display: block;
-        position: relative;
-      }
-      &.next,
-      &.prev {
-        display: block;
-        position: absolute;
-        top: 0;
-        width: 100%;
-      }
-      &.next.left,
-      &.prev.right,
-      &.active {
-        transform: translate3d(0, 0, 0);
-      }
-      &.next,
-      &.active.right {
-        transform: translate3d(100%, 0, 0);
-      }
-      &.prev,
-      &.active.left {
-        transform: translate3d(-100%, 0, 0);
-      }
-    }
-  }
 `
 
 export const FullWidth = styled.article`
