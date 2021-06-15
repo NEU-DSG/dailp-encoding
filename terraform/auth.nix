9c78933c484d7989fb8c36841742a1c07c0f60ca
Taylor Snead <taylor@snead.xyz>
2021-06-15 12:52:10 -0400

    Setup dev deployment with terraform

---
+++ terraform/auth.nix
@@ -0,0 +1,26 @@
+{ config, lib, pkgs, ... }:
+
+{
+  config.resource = {
+    aws_cognito_user_pool.main = {
+      name = "dailp-user-pool";
+      username_attributes = [ "email" ];
+      auto_verified_attributes = [ "email" ];
+      admin_create_user_config.allow_admin_create_user_only = true;
+    };
+
+    aws_cognito_user_pool_client.main = {
+      name = "dailp-user-pool-client";
+      user_pool_id = "\${aws_cognito_user_pool.main.id}";
+      allowed_oauth_flows = [ "implicit" ];
+      allowed_oauth_flows_user_pool_client = true;
+      allowed_oauth_scopes =
+        [ "email" "openid" "profile" "aws.cognito.signin.user.admin" ];
+      callback_urls = [ "http://localhost:8000" "http://localhost:9000" ];
+      explicit_auth_flows =
+        [ "ALLOW_USER_SRP_AUTH" "ALLOW_REFRESH_TOKEN_AUTH" ];
+      generate_secret = false;
+      supported_identity_providers = [ "COGNITO" ];
+    };
+  };
+}
---
+++ terraform/website.nix
@@ -0,0 +1,116 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+with builtins; {
+  config.resource = {
+    aws_iam_role.amplify_role = {
+      name = "dailp-amplify-role";
+      assume_role_policy = toJSON {
+        Statement = [{
+          Effect = "Allow";
+          Principal.Service = "amplify.amazonaws.com";
+          Action = "sts:AssumeRole";
+        }];
+        Version = "2008-10-17";
+      };
+      inline_policy = {
+        name = "Amplify";
+        policy = toJSON {
+          Statement = [{
+            Effect = "Allow";
+            Action = "amplify:*";
+            Resource = "*";
+          }];
+        };
+      };
+    };
+
+    aws_amplify_app.dailp =
+      let apiUrl = "\${aws_api_gateway_deployment.functions_api.invoke_url}";
+      in {
+        name = "dailp";
+        description =
+          "Digital Archive of American Indian Languages Preservation and Perseverance";
+        repository = getEnv "GIT_REPOSITORY_URL";
+        oauth_token = getEnv "OAUTH_TOKEN";
+        iam_service_role_arn = "\${aws_iam_role.amplify_role.arn}";
+        custom_rule = [
+          {
+            source = "/<*>";
+            status = "404-200";
+            target = "/404.html";
+          }
+          {
+            source = "/api/<*>";
+            status = "200";
+            target = apiUrl;
+          }
+          # TODO Consider removing this one and having all access be under the /api endpoint.
+          {
+            source = "/graphql";
+            status = "200";
+            target = "${apiUrl}/graphql";
+          }
+        ];
+
+        build_spec = toJSON {
+          version = "1";
+          env.variables = {
+            DAILP_GRAPHQL_URL = "${apiUrl}/graphql";
+            DAILP_AWS_REGION = config.provider.aws.region;
+            DAILP_USER_POOL = "\${aws_cognito_user_pool.main.id}";
+            DAILP_USER_POOL_CLIENT = "\${aws_cognito_user_pool_client.main.id}";
+          };
+          frontend = {
+            artifacts = {
+              baseDirectory = "website/public";
+              files = [ "**/*" ];
+            };
+            phases = {
+              build.commands = [
+                "yum install -y curl"
+                "curl https://sh.rustup.rs -sSf | sh -s -- -y"
+                "source $HOME/.cargo/env"
+                "cd website"
+                "yarn install"
+                "yarn build"
+                "cd .."
+              ];
+            };
+            cache.paths = [ "website/node_modules" "target" ];
+          };
+          customHeaders = [{
+            pattern = "**/*.{json,html}";
+            headers = [{
+              key = "Cache-Control";
+              value = "public, max-age=0, must-revalidate";
+            }];
+          }];
+        };
+      };
+
+    aws_amplify_branch = let
+      branchName = if config.setup.stage == "dev" then "dev" else "main";
+      stageName =
+        if config.setup.stage == "dev" then "DEVELOPMENT" else "PRODUCTION";
+    in {
+      current_stage = {
+        app_id = "\${aws_amplify_app.dailp.id}";
+        branch_name = branchName;
+        stage = stageName;
+        enable_auto_build = false;
+        enable_pull_request_preview = true;
+      };
+    };
+
+    aws_amplify_webhook.current_stage = {
+      app_id = "\${aws_amplify_app.dailp.id}";
+      branch_name = "\${aws_amplify_branch.current_stage.branch_name}";
+      description = "Build front-end environment";
+    };
+  };
+
+  config.output.amplify_webhook = {
+    value = "\${aws_amplify_webhook.current_stage.url}";
+  };
+}
