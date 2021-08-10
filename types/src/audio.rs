10d46de894862d84d7da7c3600866280558601a0
Naomi Trevino <trevino.n@northeastern.edu>
2021-08-10 12:49:54 -0400

    Fixed dependency issues relating to audio types
    Added audio data types to lib file
    Added empty audio fields to lexical entries (at least for now)
    added Serde serialization and GQL object to Audio types, minor name fix

--- types/src/audio.rs
+++ types/src/audio.rs
@@ -1,11 +1,15 @@
+use serde::{Deserialize, Serialize};
+
 /// Audio for a whole manuscript document
+#[derive(Serialize, Deserialize, Clone, Debug, async_graphql::SimpleObject)]
+
 pub struct DocumentAudio {
     /// Unique identifier for this audio resource
     pub id: String,
     /// Access link for the audio file.
     pub resource: String,
     /// Subdivisions of this audio track
-    pub slices: Optional<Vector<AudioSlice>>
+    pub slices: Option<Vec<AudioSlice>>
     // /// The ID for the [AnnotatedDocument] this audio resource represents
     // pub source: String,
 }
@@ -14,6 +18,7 @@ pub struct DocumentAudio {
 /// or other subunit within the audio for a full document.
 /// Slices should be associated with a [DocumentAudio] for the reading audio file
 /// this segment is a part of.
+#[derive(Serialize, Deserialize, Clone, Debug, async_graphql::SimpleObject)]
 pub struct AudioSlice {
     /// The [DocumentAudio] this audio slice is taken from
     pub parent_track: DocumentAudio,
