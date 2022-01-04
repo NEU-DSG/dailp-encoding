31e0916739f742c5d597c4cd17c7774a0a202904
Taylor Snead <taylor@snead.xyz>
2022-01-04 08:47:22 -0800

    Simplify wordpress urql import

--- website/src/graphql/wordpress/urql.ts
+++ website/src/graphql/wordpress/urql.ts
@@ -1,15 +1,13 @@
-import { useMemo } from "react"
 import { UseQueryArgs, useQuery as useQueryBase } from "urql"
 
 export type { UseQueryArgs }
 
 const WP_GRAPHQL_URL = "https://wp.dailp.northeastern.edu/graphql"
+const context = { url: WP_GRAPHQL_URL }
 
+/** Modify the default query context to use the Wordpress endpoint. */
 export function useQuery<Data = any, Variables = object>(
   params: UseQueryArgs<Variables, Data>
 ) {
-  return useQueryBase({
-    context: useMemo(() => ({ url: WP_GRAPHQL_URL }), []),
-    ...params,
-  })
+  return useQueryBase({ context, ...params })
 }
