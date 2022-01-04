3545dd87dae87000733d4ccfc3d1aa4d41287def
Taylor Snead <taylor@snead.xyz>
2022-01-04 11:06:43 -0500

    Extract wordpress page contents into component

---
+++ website/src/wordpress-page.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+import * as Wordpress from "src/graphql/wordpress"
+
+interface Props {
+  slug: string
+}
+
+const WordpressPage = ({ slug }: Props) => {
+  const [{ data, fetching }] = Wordpress.usePageQuery({
+    variables: { slug },
+  })
+  const wpPage = data?.page?.__typename === "Page" && data.page
+
+  if (wpPage) {
+    return <div dangerouslySetInnerHTML={{ __html: wpPage.content }} />
+  } else if (fetching) {
+    return (
+      <div>
+        <p>Loading...</p>
+      </div>
+    )
+  } else {
+    return null
+  }
+}
+
+export default WordpressPage
