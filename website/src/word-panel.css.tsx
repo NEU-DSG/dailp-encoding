31c675f244d915d980d2ad3c8b6de08a7edf76de
Shashwat Patel <92126390+patelShas@users.noreply.github.com>
2022-02-23 14:46:19 -0500

    Update word-panel.css.tsx

--- website/src/word-panel.css.tsx
+++ website/src/word-panel.css.tsx
@@ -1,33 +1,18 @@
-import { style } from "@vanilla-extract/css"
+import { style, styleVariants } from "@vanilla-extract/css"
+import { closeButton } from "./morpheme.css"
 import sprinkles, { hspace, mediaQueries, theme, vspace } from "./sprinkles.css"
 
-export const wholePanel = style({
-  position: "sticky",
-  top: 125,
-})
-
-export const wordPanelButton = style({
-  lineHeight: 1.5,
-  boxSizing: "border-box",
-  border: "transparent",
-  cursor: "pointer",
-  borderRadius: "0.25rem",
-  selectors: {
-    "&:before": {
-      display: "inline-block",
-      margin: "4px",
-    },
-  },
-  position: "absolute",
-  top: vspace.half,
-  right: hspace.halfEdge,
+export const wordPanelButton = styleVariants({
+  basic: [closeButton],
 })
 
 export const wordPanelContent = style({
+  position: "sticky",
+  top: 125,
+  height: "25rem",
   border: "none",
   borderRadius: "4px",
   padding: "8px",
-  position: "relative",
   fontFamily: theme.fonts.cherokee,
   "@media": {
     [mediaQueries.medium]: {
