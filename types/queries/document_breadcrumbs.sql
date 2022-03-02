7fae0ea3f857bf436a22ed347d98440272079463
Taylor Snead <taylor@snead.xyz>
2022-03-02 12:33:23 -0800

    Make some column references explicit

--- types/queries/document_breadcrumbs.sql
+++ types/queries/document_breadcrumbs.sql
@@ -1,12 +1,12 @@
 with recursive parent_tree as (
   select
-    id,
-    parent_id,
-    slug,
-    title
+    collection.id,
+    collection.parent_id,
+    collection.slug,
+    collection.title
   from collection
     inner join document_collection_position as pos on pos.document_id = $1
-  where super_collection = $2
+  where collection.super_collection = $2
     and id = pos.collection_id
   union all
   select
