f3c13e09452b1b278f63154bc0cd9c53c26cdafe
Taylor Snead <taylor@snead.xyz>
2022-04-08 10:48:46 -0700

    Provision bastion host (#160)
    Provision a bastion host to allow external access to our database, but only for IP addresses configured in the mongodb_access security group. This equates to: 1. NEU VPN, and 2. GitHub Actions runners.

---
+++ terraform/bastion-host.nix
@@ -0,0 +1,31 @@
+{ lib, config, ... }:
+{
+  options.servers.bastion = with lib; with types; {
+    instance_tags = mkOption { type = attrsOf str; };
+  };
+
+  config.module.bastion_host = {
+    source = "github.com/cloudposse/terraform-aws-ec2-bastion-server?ref=7f8fc52095ef466fedd1c06876e281a1ac6bb75b";
+    enabled = true;
+    instance_type = "t4g.micro";
+
+    # ID will be constructed from these namespace, stage, and name for some reason.
+    namespace = "dailp";
+    stage = config.setup.stage;
+    name = "bastion";
+
+    key_name = "dailp-deployment-terraform";
+
+    assign_eip_address = true;
+    associate_public_ip_address = true;
+    vpc_id = config.setup.vpc;
+    subnets = [config.setup.subnets.primary config.setup.subnets.secondary config.setup.subnets.tertiary];
+
+    # Don't create a new security group for this server.
+    security_group_enabled = false;
+    # Use the existing one setup for MongoDB access.
+    security_groups = ["\${aws_security_group.mongodb_access.id}" "\${aws_security_group.nixos_test.id}"];
+
+    tags = config.setup.global_tags // config.servers.bastion.instance_tags;
+  };
+}
