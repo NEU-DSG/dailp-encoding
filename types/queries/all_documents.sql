dc14b47df46ad3e9136ec02691a4062950101f48
Taylor Snead <taylor@snead.xyz>
2022-04-08 20:08:49 -0700

    Deduplicate listings from allDocuments query

--- types/queries/all_documents.sql
+++ types/queries/all_documents.sql
@@ -16,6 +16,4 @@ select
 from document as d
   left join contributor_attribution as attr on attr.document_id = d.id
   left join contributor on contributor.id = attr.contributor_id
-group by d.id,
-  attr.contributor_id,
-  attr.document_id
+group by d.id
