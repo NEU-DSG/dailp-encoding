d0434b998f5c04f0623e2283b793dfc29198cc83
Taylor Snead <taylor@snead.xyz>
2022-04-10 10:07:52 -0700

    Fix morpheme gloss uniqueness migration

--- types/migrations/20220410065448_unique_global_morpheme_gloss.sql
+++ types/migrations/20220410065448_unique_global_morpheme_gloss.sql
@@ -4,13 +4,20 @@
 alter table morpheme_gloss
 drop constraint morpheme_gloss_document_id_gloss_key;
 
+-- Loosen the foreign key constraint on word segments.
+-- This will allow me to delete duplicate glosses that are in use.
+alter table word_segment
+alter column gloss_id drop not null,
+drop constraint word_segment_gloss_id_fkey,
+add constraint word_segment_gloss_id_fkey
+  foreign key (gloss_id) references morpheme_gloss (id) on delete set null;
+
 -- Delete duplicate morpheme glosses.
 delete from morpheme_gloss as a
   using morpheme_gloss as b
 where a.document_id is null
   and b.document_id is null
-  and a.gloss = b.gloss
-  and not exists (select from word_segment where gloss_id = a.id);
+  and a.gloss = b.gloss;
 
 -- Force null document_id to participate in uniqueness.
 create unique index
