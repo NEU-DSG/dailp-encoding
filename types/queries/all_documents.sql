29389fee0e1078110436f97b7451d38a4d31719c
Taylor Snead <taylor@snead.xyz>
2022-04-10 00:50:32 -0700

    Make global morpheme glosses actually unique
    Fix the unique index on the morpheme_gloss table to coalesce null
    document_id's to `uuid_nil()` which will actually be checked for
    uniqueness.

--- types/queries/all_documents.sql
+++ types/queries/all_documents.sql
@@ -1,19 +1,23 @@
 select
-  d.id,
-  d.short_name,
-  d.title,
-  d.written_at,
-  d.is_reference,
-  coalesce(
-    jsonb_agg(
-      jsonb_build_object(
-        'name', contributor.full_name, 'role', attr.contribution_role
-      )
-    ) filter (where contributor is not null),
-    '[]'
-  )
-  as contributors
-from document as d
-  left join contributor_attribution as attr on attr.document_id = d.id
-  left join contributor on contributor.id = attr.contributor_id
-group by d.id
+    document.id,
+    document.short_name,
+    document.title,
+    document.written_at,
+    document.is_reference,
+    coalesce(
+        jsonb_agg(
+            jsonb_build_object(
+                'name',
+                contributor.full_name,
+                'role',
+                contributor_attribution.contribution_role
+            )
+        ) filter (where contributor is not null),
+        '[]'
+    )
+    as contributors
+from document
+left join
+    contributor_attribution on contributor_attribution.document_id = document.id
+left join contributor on contributor.id = contributor_attribution.contributor_id
+group by document.id
--- types/queries/upsert_word_segment.sql
+++ types/queries/upsert_word_segment.sql
@@ -2,15 +2,12 @@
 with global_gloss as (
   select morpheme_gloss.id
   from morpheme_gloss
-    inner join
-      abstract_morpheme_tag as abstract on morpheme_gloss.tag_id = abstract.id
   where document_id is null and gloss = $2
-  limit 1
 ), inserted_gloss as (
   insert into morpheme_gloss (document_id, gloss)
     select $1, $2
-    where not exists (select id from global_gloss)
-  on conflict (document_id, gloss)
+    where not exists (select from global_gloss)
+  on conflict (coalesce(document_id, uuid_nil()), gloss)
     do update set example_shape = excluded.example_shape,
        tag_id = excluded.tag_id
   returning id
@@ -18,7 +15,8 @@ with global_gloss as (
 
 insert into word_segment (word_id, index_in_word, morpheme, gloss_id, followed_by)
 select $3, $4, $5, coalesce(global_gloss.id, inserted_gloss.id), $6
-from global_gloss, inserted_gloss
+from global_gloss
+full outer join inserted_gloss on true
 on conflict (word_id, index_in_word)
   do update set
     morpheme = EXCLUDED.morpheme,
