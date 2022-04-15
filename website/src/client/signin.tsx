7ad4c41aff176a332954faaa611e3fb0e5c010a4
Taylor Snead <taylor@snead.xyz>
2022-04-15 12:38:07 -0700

    Remove TinaCMS and upgrade dependency minor versions

--- website/src/client/signin.tsx
+++ website/src/client/signin.tsx
@@ -1,20 +1,11 @@
 import { Auth } from "aws-amplify"
-import React, { useEffect } from "react"
-import { useCMS } from "tinacms"
+import React from "react"
 import { Button } from "src/components"
 import Link from "src/components/link"
 import { UserProvider, useCredentials } from "../auth"
 
 const ClientSignIn = () => {
   const creds = useCredentials()
-  const cms = useCMS()
-  useEffect(() => {
-    if (creds) {
-      cms.enable()
-    } else {
-      cms.disable()
-    }
-  }, [creds])
   if (creds) {
     return <Button onClick={() => Auth.signOut()}>Sign Out</Button>
   } else {
