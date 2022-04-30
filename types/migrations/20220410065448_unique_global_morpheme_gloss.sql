86f6ab1630dbb7b23dd5cd337f543a8c61607cf2
Taylor Snead <taylor@snead.xyz>
2022-04-29 19:09:02 -0700

    Revert morpheme gloss migration file
    Avoid changing this file because it causes schema migrations to fail.

--- types/migrations/20220410065448_unique_global_morpheme_gloss.sql
+++ types/migrations/20220410065448_unique_global_morpheme_gloss.sql
@@ -10,16 +10,16 @@ alter table word_segment
 alter column gloss_id drop not null,
 drop constraint word_segment_gloss_id_fkey,
 add constraint word_segment_gloss_id_fkey
-foreign key (gloss_id) references morpheme_gloss (id) on delete set null;
+  foreign key (gloss_id) references morpheme_gloss (id) on delete set null;
 
 -- Delete duplicate morpheme glosses.
 delete from morpheme_gloss as a
-using morpheme_gloss as b
+  using morpheme_gloss as b
 where a.document_id is null
   and b.document_id is null
   and a.gloss = b.gloss;
 
 -- Force null document_id to participate in uniqueness.
 create unique index
-morpheme_gloss_document_id_gloss_key on morpheme_gloss
+morpheme_gloss_document_id_gloss_key ON morpheme_gloss
 (coalesce(document_id, uuid_nil()), gloss);
