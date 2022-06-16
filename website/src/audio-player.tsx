b1efc7bce0fa2ea840a0edd1978845fd32741d3b
Shashwat <patel.shas@northeastern.edu>
2022-06-16 18:44:43 -0400

    Switched components to using <Audio/> rather than Howl.

--- website/src/audio-player.tsx
+++ website/src/audio-player.tsx
@@ -1,6 +1,6 @@
 import cx from "classnames"
 import { Howl } from "howler"
-import React, { useEffect, useMemo, useState } from "react"
+import React, { useEffect, useMemo, useRef, useState } from "react"
 import { FiLoader } from "react-icons/fi"
 import { MdPauseCircleOutline, MdPlayCircleOutline } from "react-icons/md"
 import * as Dailp from "src/graphql/dailp"
@@ -11,7 +11,7 @@ const segmentClass = cx(hideOnPrint, css.audioElement)
 export const SegmentAudio = (props: { audioUrl: string }) => {
   return (
     <span className={segmentClass}>
-      <AudioPlayer audioUrl={props.audioUrl} />
+      <AudioComponent audioUrl={props.audioUrl} />
     </span>
   )
 }
@@ -21,11 +21,18 @@ export const FormAudio = (
 ) => {
   return (
     <span>
-      <FunctionalAudioPlayer
+      {/**
+       * <FunctionalAudioPlayer
         audioUrl={props.resourceUrl}
         showProgress={!!props.showProgress}
         slices={{ start: props.startTime!, end: props.endTime! }}
         preload={true}
+      />
+       * 
+       */}
+      <AudioComponent
+        audioUrl={props.resourceUrl}
+        slices={{ start: props.startTime!, end: props.endTime! }}
       />
     </span>
   )
@@ -35,7 +42,7 @@ export const DocumentAudio = (props: { audioUrl: string }) => {
   return (
     <div className={css.wide}>
       <span>Document Audio:</span>
-      <AudioPlayer audioUrl={props.audioUrl} preload showProgress />
+      <AudioComponent audioUrl={props.audioUrl} preload showProgress />
     </div>
   )
 }
@@ -47,235 +54,93 @@ interface Props {
   slices?: { start: number; end: number }
   existingHowl?: Howl
 }
-class AudioPlayer extends React.Component<
-  Props,
-  { progress: number; howl: Howl }
-> {
-  constructor(props: Props) {
-    super(props)
-    // let slices = this.props.slices && {
-    //   ...this.props.slices.map((a) => {this.props.slices.indexOf(a)}), ...this.props.slices
-    // }
-    this.state = {
-      progress: 0,
-      howl: new Howl({
-        src: [this.props.audioUrl],
-        html5: true,
-        format: [".wav"],
-        preload: this.props.preload || false,
-        onplay: () => {
-          this.onPlay()
-        }, // What happens when a sound starts playing?
-        onend: () => {}, // What happens when a sound finishes playing?
-        onpause: () => {}, // What happens when a sound is paused?
-        onseek: () => {}, // What happens when a sound seeks?
-      }),
-    }
-    if (this.props.slices) {
-      let slices = {
-        __default: [
-          this.props.slices.start,
-          this.props.slices.end - this.props.slices.start,
-        ],
-        sound: [
-          this.props.slices.start,
-          this.props.slices.end - this.props.slices.start,
-        ],
-      }
-      Object.assign(this.state.howl, { _sprite: slices })
-    }
-    // Event handlers
-    this.nextStep = this.nextStep.bind(this)
-    this.onPlay = this.onPlay.bind(this)
-    this.onSeek = this.onSeek.bind(this)
-  }
 
-  onPlay() {
-    requestAnimationFrame(this.nextStep)
-  }
+const AudioComponent = (props: Props) => {
+  const audio = useRef(new Audio(props.audioUrl))
 
-  nextStep() {
-    let seek = this.state.howl.seek() || 0
-    this.setState((prevState, props) => ({
-      progress: (seek / this.state.howl.duration()) * 100,
-    }))
-    if (this.state.howl.playing()) {
-      requestAnimationFrame(this.nextStep)
-    }
-  }
-
-  onSeek(newProgress: number) {
-    this.setState((prevState, props) => ({
-      progress: newProgress,
-    }))
-    this.state.howl.seek((newProgress / 100) * this.state.howl.duration())
-  }
-
-  override render() {
-    let button
-    let progress = this.state.progress
-
-    if (progress > 0 && progress < 100 && this.state.howl.playing())
-      button = <PauseButton howl={this.state.howl} />
-    else
-      button = (
-        <PlayButton howl={this.state.howl} isSprite={!!this.props.slices} />
-      )
-
-    // Conditionally hide the bar
-    return (
-      <div className={css.audioElement}>
-        {button}
-        {this.props.showProgress && (
-          <ProgressBar progress={this.state.progress} seek={this.onSeek} />
-        )}
-      </div>
-    )
-  }
-}
-
-const FunctionalAudioPlayer = (props: Props) => {
   const [progress, setProgress] = useState(0)
-  const howl = useMemo(
-    () =>
-      new Howl({
-        src: [props.audioUrl],
-        html5: true,
-        format: [".wav"],
-        preload: props.preload || false,
-        onplay: () => {
-          onPlay()
-        }, // What happens when a sound starts playing?
-        onend: () => {
-          onEnd()
-        }, // What happens when a sound finishes playing?
-        onpause: () => {
-          onPause()
-        }, // What happens when a sound is paused?
-        onseek: () => {}, // What happens when a sound seeks?
-      }),
-    [props]
-  )
-  const onPlay = () => {
-    requestAnimationFrame(nextStep)
-    setIsPlaying(true)
-  }
-  const onPause = () => {
-    setIsPlaying(false)
-  }
-  const onEnd = () => {
-    setIsPlaying(false)
-  }
-  const nextStep = () => {
-    let seek = howl.seek() || 0
-    setProgress((seek / howl.duration()) * 100)
-    if (howl.playing()) {
-      requestAnimationFrame(nextStep)
-    }
-  }
-
-  if (props.slices) {
-    Object.assign(howl, {
-      _sprite: {
-        __default: [props.slices.start, props.slices.end - props.slices.start],
-        sound: [props.slices.start, props.slices.end - props.slices.start],
-      },
-    })
-  }
-
-  const onSeek = (newProgress: number) => {
-    setProgress(newProgress)
-    howl.seek((newProgress / 100) * howl.duration())
-  }
-
-  // Button set up
-  const [isPlaying, setIsPlaying] = useState(false)
-  const [soundID, setSoundID] = useState<number>()
-  const startPlay = () => {
-    let spriteID = howl.play(soundID ? soundID : "sound")
-    setSoundID(spriteID)
-  }
-  const pausePlay = () => {
-    howl.pause(soundID)
-  }
-  const pauseButton = (
-    <MdPauseCircleOutline size={buttonSize} onClick={pausePlay} />
-  )
-  const playButton = (
-    <MdPlayCircleOutline size={buttonSize} onClick={startPlay} />
-  )
-  const loadIcon = <FiLoader size={buttonSize} />
-  const button = isPlaying
-    ? pauseButton
-    : howl.state() == "loading"
-    ? loadIcon
-    : playButton
+  const [loadStatus, setLoadStatus] = useState(0)
 
   useEffect(() => {
-    props.slices ? howl.seek(props.slices.start / 1000) : null
+    const interval = setInterval(() => {
+      setProgress(audio.current.currentTime)
+      setLoadStatus(audio.current.readyState)
+    }, 100)
+    return () => clearInterval(interval)
+  }, [])
+
+  const [start, end] = props.slices
+    ? [props.slices.start / 1000, props.slices.end / 1000]
+    : [0, audio.current.duration]
+
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  useEffect(() => {
+    if (isPlaying) {
+      audio.current.play()
+      console.log("bruh")
+    } else {
+      audio.current.pause()
+    }
+  }, [isPlaying])
+
+  const setPlayStart = () => {
+    audio.current.currentTime = props.slices
+      ? (audio.current.currentTime = props.slices.start / 1000)
+      : 0
+  }
+
+  useEffect(() => {
+    setPlayStart()
   }, [props.slices])
 
-  const bounds = props.slices
-    ? {
-        start: props.slices.start / (howl.duration() * 10),
-        end: props.slices.end / (howl.duration() * 10),
-      }
-    : null
+  useEffect(() => {
+    if (progress >= end) {
+      setIsPlaying(false)
+      setPlayStart()
+    }
+  }, [progress])
 
-  // Finally return call
-  // Conditionally hide the bar
   return (
-    <div className={css.audioElement}>
-      {button}
-      {props.showProgress && (
-        <ProgressBar progress={progress} seek={onSeek} bounds={bounds} />
+    <div className="audio-controls">
+      {loadStatus >= audio.current.HAVE_FUTURE_DATA ? (
+        isPlaying ? (
+          <MdPauseCircleOutline
+            size={buttonSize}
+            onClick={() => setIsPlaying(false)}
+            aria-label="Pause"
+          />
+        ) : (
+          <MdPlayCircleOutline
+            size={buttonSize}
+            onClick={() => setIsPlaying(true)}
+            aria-label="Play"
+          />
+        )
+      ) : (
+        <FiLoader size={buttonSize} />
       )}
+      <ProgressBar progress={progress} bounds={{ start, end }} />
     </div>
   )
 }
 
 const buttonSize = 28
 
-const PauseButton = (props: { howl: Howl }) => {
-  return (
-    <MdPauseCircleOutline
-      size={buttonSize}
-      onClick={() => props.howl.pause()}
-    />
-  )
-}
-
-const PlayButton = (props: { howl: Howl; isSprite?: boolean }) => {
-  return props.isSprite ? (
-    <MdPlayCircleOutline
-      size={buttonSize}
-      onClick={() => props.howl.play("sound")}
-    />
-  ) : (
-    <MdPlayCircleOutline size={buttonSize} onClick={() => props.howl.play()} />
-  )
-}
-// TODO: Implement Seek
-// TODO: Implement start and end times
 const ProgressBar = (props: {
   progress: number
-  seek: (n: number) => void
-  bounds?: { start: number; end: number } | null
+  bounds: { start: number; end: number } | null
 }) => {
-  let progPtr = props.progress
-  let onClick = () => {
-    props.seek(3)
-  }
-  if (props.bounds) {
-    progPtr = Math.min(
-      100,
-      (100 * (props.progress - props.bounds.start)) /
-        (props.bounds.end - props.bounds.start)
-    )
-    onClick = () => {} // So word audio doesn't get messed by clicks
-  }
+  const progPtr =
+    props.bounds && props.bounds.start != Infinity
+      ? Math.min(
+          100,
+          (100 * (props.progress - props.bounds.start)) /
+            (props.bounds.end - props.bounds.start)
+        )
+      : 0
   return (
-    <div className={css.container} onClick={onClick} aria-label={"container"}>
+    <div className={css.container} aria-label={"container"}>
       <div
         className={css.fill}
         style={{ width: `${progPtr}%` }}
