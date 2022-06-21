9c1925e0143f40ac7c0e75b493d51d3d14e503ee
Taylor Snead <taylor@snead.xyz>
2022-06-21 16:38:27 -0700

    Move website/src/components => website/src/components/index

--- website/src/components.tsx
+++
@@ -1,3 +0,0 @@
-export { Button, IconButton, CleanButton } from "./components/button"
-export { default as Link } from "./components/link"
-export { Select, Label } from "./components/select"
---
+++ website/src/components/index.ts
@@ -0,0 +1,3 @@
+export { Button, IconButton, CleanButton } from "./button"
+export { default as Link } from "./link"
+export { Select, Label } from "./select"
