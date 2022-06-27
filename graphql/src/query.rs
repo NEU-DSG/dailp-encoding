95770ea990a0967a0d20a6656aa05e55b3917e16
chanmioh <chanmioh@gmail.com>
2022-06-27 13:56:58 -0400

    Imported MaybeUndefined, ran dev-generate-types

--- graphql/src/query.rs
+++ graphql/src/query.rs
@@ -1,5 +1,6 @@
 //! This piece of the project exposes a GraphQL endpoint that allows one to access DAILP data in a federated manner with specific queries.
 
+use dailp::Uuid;
 use itertools::Itertools;
 
 use {
