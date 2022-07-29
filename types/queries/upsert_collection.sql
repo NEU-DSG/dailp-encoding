fba6843e19cb1d992879a3ba4abeab8b242a189b
Taylor Snead <taylor@snead.xyz>
2022-07-29 14:42:05 -0700

    Run sqlfluff

--- types/queries/upsert_collection.sql
+++ types/queries/upsert_collection.sql
@@ -1,5 +1,5 @@
 insert into edited_collection (slug, title, wordpress_menu_id)
 values ($1, $2, $3)
-on conflict (slug) do update 
+on conflict (slug) do update
 set title = excluded.title,
-    wordpress_menu_id = excluded.wordpress_menu_id
+wordpress_menu_id = excluded.wordpress_menu_id
