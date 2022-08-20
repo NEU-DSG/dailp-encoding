9fe89c63db2dc7fc47b2d69ffb55a31ea685a414
Taylor Snead <taylor@snead.xyz>
2022-08-20 09:34:51 -0700

    Remove git operations from dev-check
    Let dev-check be a tool for unstaged files too, simply grouping all of
    the formatting, code generation, and type-checking steps into one command.

--- check.sh
+++ check.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 
-git-staged() {
-    ! git diff --no-patch --staged --exit-code "$PROJECT_ROOT/$1"
-}
-
 cd $PROJECT_ROOT
 
 echo "--- DATABASE ---"
-if git-staged "types/queries/**.sql"; then
+echo "Checking if SQL types are up to date..."
+if $(cd types; cargo sqlx prepare --check &> /dev/null); then
     echo "Generating SQL types..."
-    dev-generate-types || exit 1
-    git add sqlx-data.json
+    cargo sqlx prepare -- -p dailp || exit 1
 fi
 
 echo "--- SERVER ---"
@@ -24,7 +20,6 @@ cargo check &> /dev/null \
 echo "Generating GraphQL schema..."
 cargo run --bin dailp-graphql-schema &> /dev/null \
     || (echo "GraphQL server build failed, run 'cargo check' for details." && exit 1)
-git add graphql/schema.graphql
 
 echo "--- WEBSITE ---"
 cd website
@@ -32,7 +27,6 @@ echo "Formatting TypeScript code..."
 yarn prettier --write src &> /dev/null
 echo "Generating Typescript types for GraphQL queries..."
 yarn generate
-git add src/graphql/*/index.ts
 echo "Checking website for errors..."
 yarn tsc || exit 1
 
@@ -40,4 +34,6 @@ echo "--- FINAL CHECKS ---"
 cd $PROJECT_ROOT
 ./.git-hooks/pre-commit || exit 1
 
-echo "TODO: Please stage relevant formatting changes"
+echo
+echo "--- NEXT STEPS ---"
+echo "Please stage relevant automatic changes"
