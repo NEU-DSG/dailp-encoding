234f2242d1e0413bb09a289dd99f8833c37b5cf9
Taylor Snead <taylor@snead.xyz>
2022-09-08 15:01:22 -0700

    Upgrade @graphql-codegen/cli to resolve security warnings
    Dependabot was warning about several dependencies of this dev tool, so I
    upgraded it to the latest version hoping to resolve them.

--- check.sh
+++ check.sh
@@ -4,27 +4,27 @@ cd $PROJECT_ROOT
 
 echo "--- DATABASE ---"
 echo "Checking if SQL types are up to date..."
-if $(cd types; cargo sqlx prepare --check &> /dev/null); then
+if $(cd types && cargo sqlx prepare --check &>/dev/null); then
     echo "Generating SQL types..."
     cargo sqlx prepare -- -p dailp || exit 1
 fi
 
 echo "--- SERVER ---"
 echo "Formatting Rust code..."
-cargo fmt &> /dev/null
+cargo fmt &>/dev/null
 echo "Checking back-end for errors..."
 export SQLX_OFFLINE=true
-cargo check &> /dev/null \
-    || (echo "Back-end build failed, run 'cargo check' for details." && exit 1)
+cargo check &>/dev/null ||
+    (echo "Back-end build failed, run 'cargo check' for details." && exit 1)
 
 echo "Generating GraphQL schema..."
-cargo run --bin dailp-graphql-schema &> /dev/null \
-    || (echo "GraphQL server build failed, run 'cargo check' for details." && exit 1)
+cargo run --bin dailp-graphql-schema &>/dev/null ||
+    (echo "GraphQL server build failed, run 'cargo check' for details." && exit 1)
 
 echo "--- WEBSITE ---"
 cd website
 echo "Formatting TypeScript code..."
-yarn prettier --write src &> /dev/null
+yarn prettier --write src &>/dev/null
 echo "Generating Typescript types for GraphQL queries..."
 yarn generate
 echo "Checking website for errors..."
