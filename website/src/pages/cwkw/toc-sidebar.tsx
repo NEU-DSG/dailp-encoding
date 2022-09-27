347523733ee94d1ce8cc5db3bb45ce8a9fe7cb8f
chanmi <40529597+chanmioh@users.noreply.github.com>
2022-09-27 17:17:23 -0400

    [DP-172] Add home page for CWKW collection at "/cwkw" (#217)
    * Organized CWKW files and fleshed out CWKW homepage.

---
+++ website/src/pages/cwkw/toc-sidebar.tsx
@@ -0,0 +1,51 @@
+import React from "react"
+import { BsArrowBarLeft, BsArrowBarRight } from "react-icons/bs"
+import { IconButton } from "src/components"
+import Tox1 from "src/components/toc.page"
+import * as css from "./toc-sidebar.css"
+
+// Renders a sidebar on the left side of the screen that displays a table of contents.
+const TOCSidebar = (p: {
+  isOpen: boolean
+  setIsOpen: (bool: boolean) => void
+}) => {
+  return (
+    // If the sidebar is open, give its container more space in the flexbox.
+    <div style={{ flex: p.isOpen ? 2 : 1 }}>
+      <nav className={css.sidebar}>
+        {/* TOC Header  */}
+        <div className={css.tocHeader}>
+          {p.isOpen ? (
+            // If the panel is open, table of contents header and a button to close.
+            // NOTE: The close button will not appear on mobile.
+            <>
+              <div style={{ paddingLeft: "1rem" }}>Table of Contents</div>
+              <IconButton
+                onClick={() => {
+                  p.setIsOpen(false)
+                }}
+                aria-label="Dismiss Table of Contents Panel"
+              >
+                <BsArrowBarLeft size={24} color="#E2E2E2" />
+              </IconButton>
+            </>
+          ) : (
+            // If the panel is closed, show a button to open.
+            <IconButton
+              onClick={() => {
+                p.setIsOpen(true)
+              }}
+              aria-label="Open Table of Contents Panel"
+            >
+              <BsArrowBarRight size={24} color="#E2E2E2" />
+            </IconButton>
+          )}
+        </div>
+        {/* If the panel is open, show the chapters content. */}
+        {p.isOpen && <Tox1 />}
+      </nav>
+    </div>
+  )
+}
+
+export default TOCSidebar
