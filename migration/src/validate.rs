7be8eff1ef95dd89bf4a27452d9d2450bb2efaa7
Taylor Snead <taylor@snead.xyz>
2022-11-30 13:41:53 -0800

    Split data validation into separate binary (#236)

--- migration/src/validate.rs
+++ migration/src/validate.rs
@@ -24,89 +24,59 @@ async fn main() -> Result<()> {
     dotenv::dotenv().ok();
     pretty_env_logger::init();
 
-    let db = Database::connect(Some(1))?;
+    println!("Validating manuscript spreadsheets...");
+    validate_documents().await?;
 
-    println!("Migrating Image Sources...");
-    migrate_image_sources(&db).await?;
-
-    println!("Migrating contributors...");
-    contributors::migrate_all(&db).await?;
-
-    println!("Migrating tags to database...");
-    tags::migrate_tags(&db).await?;
-
-    println!("Migrating DF1975 and DF2003...");
-    lexical::migrate_dictionaries(&db).await?;
-
-    println!("Migrating early vocabularies...");
-    early_vocab::migrate_all(&db).await?;
-
-    migrate_data(&db).await?;
-
-    println!("Migrating connections...");
-    connections::migrate_connections(&db).await?;
-
-    println!("Migrating collections...");
-    edited_collection::migrate_edited_collection(&db).await?;
-
-    Ok(())
-}
-
-async fn migrate_image_sources(db: &Database) -> Result<()> {
-    db.upsert_image_source("beinecke", "https://collections.library.yale.edu/iiif/2")
-        .await?;
-    db.upsert_image_source(
-        "dailp",
-        "https://wd0ahsivs3.execute-api.us-east-1.amazonaws.com/latest/iiif/2",
-    )
-    .await?;
     Ok(())
 }
 
 /// Parses our annotated document spreadsheets, migrating that data to our
 /// database and writing them into TEI XML files.
-async fn migrate_data(db: &Database) -> Result<()> {
+async fn validate_documents() -> Result<()> {
     // Pull the list of annotated documents from our index sheet.
     let index =
         spreadsheets::SheetResult::from_sheet("1sDTRFoJylUqsZlxU57k1Uj8oHhbM3MAzU8sDgTfO7Mk", None)
             .await?
             .into_index()?;
 
-    println!("Migrating documents to database...");
-
     // Retrieve data for spreadsheets in sequence.
     // Because of Google API rate limits, we have to process them sequentially
     // rather than in parallel.
     // This process encodes the ordering in the Index sheet to allow us to
     // manually order different document collections.
-    let mut morpheme_relations = Vec::new();
-    let mut document_contents = Vec::new();
-    for (coll_index, collection) in index.collections.into_iter().enumerate() {
-        let collection_id = db
-            .insert_top_collection(collection.title, coll_index as i64)
-            .await?;
+    let mut results = Vec::new();
+    for collection in index.collections {
+        let collection_id = Default::default();
         for (order_index, sheet_id) in collection.sheet_ids.into_iter().enumerate() {
-            if let Some((doc, mut refs)) =
-                fetch_sheet(Some(db), &sheet_id, collection_id, order_index as i64).await?
-            {
-                morpheme_relations.append(&mut refs);
-                // spreadsheets::write_to_file(&doc)?;
-                document_contents.push(doc);
-            } else {
+            results.push((
+                sheet_id.clone(),
+                fetch_sheet(None, &sheet_id, collection_id, order_index as i64).await,
+            ));
+            tokio::time::sleep(Duration::from_millis(1300)).await;
+        }
+    }
+
+    let mut failed = false;
+    for (sheet_id, r) in results {
+        match r {
+            Err(e) => {
+                error!("Failed to process {}: {}", sheet_id, e);
+                failed = true;
+            }
+            Ok(None) => {
                 error!("Failed to process {}", sheet_id);
+            }
+            Ok(Some(_)) => {
+                info!("{} validated", sheet_id);
             }
         }
     }
 
-    // Each document takes a bunch of operations to insert, so do them
-    // sequentially instead of concurrently to avoid starving the tasks.
-    for doc in document_contents {
-        db.insert_document_contents(doc).await?;
+    if failed {
+        Err(anyhow::anyhow!("One or more documents failed to process"))
+    } else {
+        Ok(())
     }
-
-    db.insert_morpheme_relations(morpheme_relations).await?;
-
-    Ok(())
 }
 
 /// Fetch the contents of the sheet with the given ID, validating the first page as
