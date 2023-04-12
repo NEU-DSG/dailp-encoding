a51a357a0e9552f282cbf1efd56cf56a6e3fb02a
Naomi Trevino <trevino.n@northeastern.edu>
2023-04-12 11:15:40 -0700

    Fallback to default values when env var not found

--- migration/src/audio.rs
+++ migration/src/audio.rs
@@ -4,20 +4,17 @@ use log::{error, info};
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use std::{
-    collections::{HashMap},
-    time::Duration,
-};
+use std::{collections::HashMap, time::Duration};
 use tokio::time::sleep;
 
 /// Represents an identifier from Northeastern's Digital Repository Service (DRS)
-/// 
+///
 /// DRS identifiers contain a 9-digit base-36 number prefixed with "neu:".
 /// An ID can refer to any kind of DSG object, including collections,
 /// object containers, and raw data (such as text, audio, or video).
-/// 
+///
 /// Examples:
-/// 
+///
 /// The DRS ID for a collection, ...
 /// ```
 /// let drs_collection_id = DrsId("neu:ww72bh375")
@@ -27,12 +24,12 @@ use tokio::time::sleep;
 /// ```
 /// let drs_audio_container_id = DrsId("neu:2r36v5312")
 /// ```
-/// 
+///
 /// ... for raw audio data, ...
 /// ```
 /// let drs_audio_data_id = DrsId("neu:4f17r7213")
 /// ```
-/// 
+///
 /// ... and for raw text data.
 /// ```
 /// let drs_text_data_id = DrsId("neu:bz613v117")
@@ -41,27 +38,27 @@ use tokio::time::sleep;
 struct DrsId(String);
 
 /// Represents key-value pairs used to structure DRS data.
-/// 
+///
 /// Currently, only supports data in the form `"url": "value"`,
 /// which is used for encoding the canonical object and content objects for a DRS entry.
 /// Note that [ComplexDrsObject]s are a tuple struct wrapping [HashMap].
-/// 
+///
 /// Examples:
-/// 
+///
 /// The following two examples assume an HTTP response with the following JSON snippet:
 /// ```text
 /// {
 ///     "https://repository.library.northeastern.edu/downloads/neu:4f18fk930?datastream_id=content": "Audio File"
 /// }
 /// ```
-/// 
+///
 /// Accessing data stored in a DRS Object
 /// ```
 /// # let body = "{'https://repository.library.northeastern.edu/downloads/neu:4f18fk930?datastream_id=content': 'Audio File'}";
 /// # let some_drs_object = ComplexDrsObject(body);
 /// let drs_object_data : HashMap<String, Value> = some_drs_object.0;
 /// ```
-/// 
+///
 /// Accessing a specific entry url
 /// ```
 /// # let body = "{'https://repository.library.northeastern.edu/downloads/neu:4f18fk930?datastream_id=content': 'Audio File'}";
@@ -69,7 +66,7 @@ struct DrsId(String);
 /// let drs_data_url : String = drs_object_data.keys().next();
 /// println!("{}", drs_data_url);
 /// ```
-/// 
+///
 /// Responses can theoretically have a list of any length that we could want to parse, as shown below.
 /// ```text
 /// {
@@ -86,17 +83,17 @@ impl ComplexDrsObject {
     ///
     /// This is most commonly used to retrieve the download url to a canonical resource associated
     /// with an audio file or annotation file.
-    /// 
+    ///
     /// Example:
-    /// 
+    ///
     /// ```
     /// # let body = "{'https://repository.library.northeastern.edu/downloads/neu:4f18fk930?datastream_id=content': 'Audio File'}";
     /// # let some_drs_object = ComplexDrsObject(body);
     /// download_url = some_drs_object.get_first_object_pid();
     /// println!("{}", download_url);
     /// ```
-    /// 
-    /// Note: the example above demonstrates expected behavior for the JSON 
+    ///
+    /// Note: the example above demonstrates expected behavior for the JSON
     /// ```text
     /// {
     ///     "https://repository.library.northeastern.edu/downloads/neu:4f18fk930?datastream_id=content": "Audio File"
@@ -124,14 +121,14 @@ struct DrsRes {
 
 impl DrsRes {
     /// Creates a new DRS resource
-    /// 
+    ///
     /// # Errors
     /// Fails if a connection with the DRS can not be established in a set number of tries
     /// or if the response body cannot be deserialized properly.
     pub async fn new(client: &Client, drs_id: &str) -> Result<Self, anyhow::Error> {
         let drs = "https://repository.library.northeastern.edu/api/v1/files/";
         let mut tries = 0;
-        
+
         Ok(loop {
             let r = client
                 .get(format!("{}{}", drs, drs_id))
@@ -154,8 +151,8 @@ impl DrsRes {
 }
 
 /// Represents annotation info associated with a single word
-/// 
-/// 
+///
+///
 #[derive(Serialize, Deserialize, Clone, Debug)]
 struct AudioAnnotationRow {
     layer: Option<String>,
@@ -165,17 +162,17 @@ struct AudioAnnotationRow {
 }
 
 /// Represents an audio resource and its annotations from an online source.
-/// 
+///
 /// At this moment, each [AudioRes] _must_ have annotations present.
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct AudioRes {
     /// The URL associated with an audio resource.
-    /// 
+    ///
     /// Currently, this URL will be from either the DRS or an S3 bucket.
     audio_url: String,
 
     /// Text holding start and end times for words contained within an audio file
-    /// 
+    ///
     /// Example Contents:
     /// ```text
     /// ᏓᎬᏃᎯᏎᎵ  6.26    6.39
@@ -187,16 +184,14 @@ pub struct AudioRes {
 
 impl AudioRes {
     /// Creates a new Audio Resource from an audio key and an annotaion key.
-    /// 
+    ///
     /// Keys should either be a resource string from the DRS or DAILP audio S3 Bucket.
     /// As of now, both keys must be from the same source (DRS or S3).
 
     pub async fn new(audio_ref_key: &str, annotation_ref_key: &str) -> Result<Self, anyhow::Error> {
         println!("Creating new Audio Resource...");
         let client = Client::new();
-        let is_drs_key = | test_value: &str| -> bool {
-            println!("{}", test_value);
-
+        let is_drs_key = |test_value: &str| -> bool {
             return test_value.contains("neu");
         };
 
@@ -206,51 +201,60 @@ impl AudioRes {
             let audio_response = DrsRes::new(&client, audio_ref_key).await?;
             let annotation_response = DrsRes::new(&client, annotation_ref_key).await?;
 
-            Ok(Self { 
+            Ok(Self {
                 audio_url: audio_response.get_url(),
-                annotations: Self::get_http_body(&client, annotation_response.get_url()).await?
+                annotations: Self::get_http_body(&client, annotation_response.get_url()).await?,
             })
         } else {
             println!("Found S3 Location");
             dotenv::dotenv().ok();
-            let deploy_env = std::env::var("VITE_DEPLOYMENT_ENV")?;
-            let aws_region = std::env::var("DAILP_AWS_REGION")?;
-            let s3_location = 
-                format!("https://dailp-{}-media-storage.s3.{}.amazonaws.com",
-                if deploy_env.eq("local") {String::from("dev")} else {deploy_env},
-                aws_region);
+            let deploy_env = std::env::var("TF_STAGE")
+                .ok()
+                .unwrap_or_else(|| String::from("dev"));
+
+            let aws_region = std::env::var("AWS_ZONE_PRIMARY")
+                .ok()
+                .unwrap_or_else(|| String::from("us-east-1"));
+
+            let s3_location = format!(
+                "https://dailp-{}-media-storage.s3.{}.amazonaws.com",
+                deploy_env, aws_region
+            );
 
             println!("{}{}", s3_location, audio_ref_key);
 
             Ok(Self {
                 audio_url: format!("{}{}", s3_location, audio_ref_key).to_owned(),
-                annotations:
-                Self::get_http_body(&client, format!("{}{}", s3_location, annotation_ref_key)).await?
+                annotations: Self::get_http_body(
+                    &client,
+                    format!("{}{}", s3_location, annotation_ref_key),
+                )
+                .await?,
             })
         }
     }
-    
+
     /// Tries to get the body response for a GET request to the provided url
-    /// 
+    ///
     /// # Errors
     /// This method fails when:
     /// - the URL is malformed
     /// - no HTTP request could be made
     /// - no HTTP response body is present
-    /// 
+    ///
     /// # Examples
     /// Failure
     /// ```
     /// # let _client = Client::new()
     /// get_http_body(client, String::new("https://www.examp;e.com/my-resource.csv"))
     /// ```
-    /// 
+    ///
     async fn get_http_body(client: &Client, url: String) -> Result<String, anyhow::Error> {
         Ok(client.get(url).send().await?.text().await?)
     }
 
     /// Converts this [AudioRes] into an [AudioSlice] representing audio for an entire document
-    /// 
+    ///
     /// Note: Documents cannot have parent tracks at this time.
     pub fn into_document_audio(self) -> AudioSlice {
         AudioSlice {
@@ -264,9 +268,9 @@ impl AudioRes {
     }
 
     /// Converts this [AudioRes] to [AudioSlice]s based on segmentations in [AudioRes]' annotation field
-    /// 
+    ///
     /// Each row in the annotation field is encoded as one [AudioSlice], representing one word.
-    /// 
+    ///
     /// # Errors
     /// Fails if annotation field does not meet structural requirements or is empty.
     pub fn into_audio_slices(self /*from_layer: String*/) -> Vec<AudioSlice> {
@@ -279,8 +283,7 @@ impl AudioRes {
             .has_headers(false)
             .from_reader(self.annotations.as_bytes());
 
-        for (annotation_line, i) in reader.deserialize::<AudioAnnotationRow>().zip(0..)
-        {
+        for (annotation_line, i) in reader.deserialize::<AudioAnnotationRow>().zip(0..) {
             if annotation_line.is_err() {
                 error!("Failed to add line {}", i);
                 result.push(AudioSlice {
@@ -313,4 +316,3 @@ impl AudioRes {
         result
     }
 }
-
