84ced1c5e481c6efacc5e87b9e097f2084fdd22d
Naomi Trevino <trevino.n@northeastern.edu>
2023-05-11 15:31:07 -0400

    Configue Cloudfront for content access

---
+++ terraform/media-access.nix
@@ -0,0 +1,42 @@
+{ config, lib, pkgs, ...} : {
+
+config.resource = {
+  aws_cloudfront_origin_access_control.media_access_control = {
+    name = "TEST-dailp-${config.setup.stage}-media-access-control";
+    description = "test cloudfront for dailp s3 access via cognito";
+    origin_access_control_origin_type = "s3";
+    signing_behavior = "always";
+    signing_protocol = "sigv4";
+  };
+
+  # Provision a CloudForm Distribution to manage Read/Write operations to S3
+  aws_cloudfront_distribution.media_distribution = {
+    enabled = true;
+    origin = {
+      domain_name = media_storage.bucket_regional_domain_name;
+      origin_id = media_storage.id;
+      origin_access_control_id = media_access_control.id;
+    };
+    default_cache_behavior = {
+      allowed_methods = ["DELETE" "GET" "HEAD" "OPTIONS" "PATCH" "POST" "PUT"];
+      cached_methods = ["GET" "HEAD"];
+      target_origin_id = media_storage.id;
+      viewer_protocol_policy = "redirect-to-https";
+      cache_policy_id = data.media-cache-policy.id;
+    };
+    restrictions = {
+      geo_restriction = {
+        restriction_type = "none";
+        locations = []
+      };
+    };
+    viewer_certificate = {
+      cloudfront_default_certificate = true;
+    };
+  };
+};
+
+  config.data.aws_cloudfront_cache_policy.media-cache-policy = {
+    name = "TEST-dailp-${config.setup.stage}-media-cache-policy";
+  };
+}
