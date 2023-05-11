cd5535aaa814ad7acbde39b6d305b2c2be415618
Naomi Trevino <trevino.n@northeastern.edu>
2023-05-11 15:30:05 -0400

    Add s3 media storage policy

--- terraform/media-storage.nix
+++ terraform/media-storage.nix
@@ -1,22 +1,28 @@
 { config, lib, pkgs, ... }: {
-  # Provision a bucket dedicated to media storage, especially audio files.
-  config.resource.aws_s3_bucket.media_storage = {
-    bucket = "dailp-${config.setup.stage}-media-storage";
-    tags = config.setup.global_tags;
-    acl = "private";
-    lifecycle.prevent_destroy = true;
-    # We don't need S3 to retain old versions of our media files, especially
-    # since NU takes backups anyway (I think).
-    versioning.enabled = false;
 
-    # Copied the rest from the bootstrap bucket.
-    logging = {
-      target_bucket = config.setup.access_log_bucket;
-      target_prefix = "/dailp-${config.setup.stage}-media-storage";
-    };
-    server_side_encryption_configuration.rule.apply_server_side_encryption_by_default =
+  # Provision a bucket dedicated to media storage, especially audio files.
+  config.resource = {
+    aws_s3_bucket.media_storage = {
+      bucket = "TEST-dailp-${config.setup.stage}-media-storage";
+      tags = config.setup.global_tags;
+      acl = "private";
+      lifecycle.prevent_destroy = true;
+      versioning.enabled = true;
+
+      # Copied the rest from the bootstrap bucket.
+      logging = {
+        target_bucket = config.setup.access_log_bucket;
+        target_prefix = "/TEST-dailp-${config.setup.stage}-media-storage";
+      };
+      server_side_encryption_configuration.rule.apply_server_side_encryption_by_default =
       {
         sse_algorithm = "AES256";
       };
+    };
+  };
+    
+  aws_s3_bucket_policy.media_storage_policy = {
+    bucket = aws_s3_bucket.media_storage.id;
+    policy = data.aws_iam_policy_document.[...].json;
   };
 }
