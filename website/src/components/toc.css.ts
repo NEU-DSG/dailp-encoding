a4b4acca470da1a5056868ee50bf21a84ac54a78
Naomi Trevino <trevino.n@northeastern.edu>
2023-05-25 21:05:11 -0400

    Remove CC-BY from CWKW sidebar

--- website/src/components/toc.css.ts
+++ website/src/components/toc.css.ts
@@ -2,7 +2,6 @@ import { style } from "@vanilla-extract/css"
 import { margin } from "polished"
 import { colors, hsize, hspace, vspace } from "src/style/constants"
 import { marginY, onHover, paddingX, paddingY } from "src/style/utils"
-import { linkColor } from "src/components/link.css"
 
 
 // Styles intro chapters.
@@ -83,14 +82,4 @@ export const title = style([
     color: colors.primaryText,
     fontWeight: "bolder",
   },
-])
-
-export const noticeText = style([
-  { paddingTop: vspace.large,
-    borderTop: `1px ridge ${colors.borders}`,
-    color: colors.primaryText,
-    vars: {
-      [linkColor]: "white",
-      [colors.focus]: "white",
-    },}
 ])
