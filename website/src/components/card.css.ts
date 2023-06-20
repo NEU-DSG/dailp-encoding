27f019aa196567ff6847b4ba8647dc2d02ab559c
Naomi Trevino <trevino.n@northeastern.edu>
2023-06-20 17:07:10 -0400

    Add detail card for CWKW

---
+++ website/src/components/card.css.ts
@@ -0,0 +1,27 @@
+import { style } from "@vanilla-extract/css"
+
+export const card = style({
+    width: "100%",
+    borderWidth: "1px",
+    borderStyle: "solid",
+    borderColor: "black",
+    borderRadius: "1em",
+    display: "flex"
+  })
+
+  export const cardImage = style({
+    width: "25%",
+    borderBottomLeftRadius: "inherit",
+    borderTopLeftRadius: "inherit",
+    margin: "0px"
+  })
+
+  export const cardHeader = style({
+
+  })
+
+  export const cardText = style({
+    marginLeft: "2em",
+    marginTop: "2em",
+    marginRight:"1em"
+  })
