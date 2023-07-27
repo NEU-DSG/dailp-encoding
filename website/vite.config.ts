4847597ccb01bf830bcfc099dffe0e4bafbdde01
Charlie McVicker <charlie@mcvickernet.com>
2023-07-27 15:24:50 -0400

    [test] disable prerendering

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,11 +1,11 @@
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin"
 import react from "@vitejs/plugin-react"
+import os from "node:os"
 import postcssPresetEnv from "postcss-preset-env"
 import { defineConfig } from "vite"
 import checker from "vite-plugin-checker"
 import ssr from "vite-plugin-ssr/plugin"
 import tsconfigPaths from "vite-tsconfig-paths"
-import os from "node:os"
 
 export default defineConfig({
   plugins: [
@@ -13,11 +13,14 @@ export default defineConfig({
     tsconfigPaths(),
     vanillaExtractPlugin(),
     // Only check types in development mode.
-    process.env.NODE_ENV === "development" ? checker({ typescript: true }) : undefined,
+    process.env.NODE_ENV === "development"
+      ? checker({ typescript: true })
+      : undefined,
     ssr({
-      prerender: {
-        parallel: Math.min(4, os.cpus().length)
-      }
+      prerender: false,
+      // prerender: {
+      //   parallel: Math.min(4, os.cpus().length)
+      // }
     }),
   ],
   css: {
