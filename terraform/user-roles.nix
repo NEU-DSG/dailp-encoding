9708db9e4ec3908cc3df3fea7d7511d96f93ae05
Naomi Trevino <trevino.n@northeastern.edu>
2023-09-20 20:39:27 -0400

    Attach S3 bucket policy

--- terraform/user-roles.nix
+++ terraform/user-roles.nix
@@ -9,7 +9,7 @@
         resources = ["bucket arn"];
         principals = {
           type = "AWS";
-          identifiers = [ config.resources.aws_iam_role.basic_user_role.arn ];
+          identifiers = [ "$\{config.resource.aws_iam_role.basic_user_role.arn}" ];
         };
       };
     };
@@ -21,7 +21,7 @@
         resources = ["bucket arn"];
         principals = {
           type = "AWS";
-          identifiers = [ config.resources.aws_iam_role.contributor_user_role.arn ];
+          identifiers = [ "$\{config.resource.aws_iam_role.contributor_user_role.arn}" ];
         };
       };
     };
@@ -33,7 +33,7 @@
         resources = ["bucket arn"];
         principals = {
           type = "AWS";
-          identifiers = [ config.resources.aws_iam_role.editor_user_role.arn ];
+          identifiers = [ "$\{config.resource.aws_iam_role.editor_user_role.arn}" ];
         };
       };
     };
@@ -42,19 +42,19 @@
   # Role Definitions
   config.resource.aws_iam_role = {
     basic_user_role = {
-      assume_role_policy = config.data.aws_iam_policy_document.basic_user_policy.json;
+      assume_role_policy = "$\{config.data.aws_iam_policy_document.basic_user_policy.json}";
       description = "A basic DAILP user with read only s3 access";
       name = "dailp-basic-user-role";
     };
 
     contributor_user_role = {
-      assume_role_policy = config.data.aws_iam_policy_document.contributor_user_policy.json;
+      assume_role_policy = "$\{config.data.aws_iam_policy_document.contributor_user_policy.json}";
       description = "A DAILP contributor with limited read and write s3 access";
       name = "dailp-contributor-user-role";
     };
 
     editor_user_role = {
-      assume_role_policy = config.data.aws_iam_policy_document.editor_user_policy.json;
+      assume_role_policy = "$\{config.data.aws_iam_policy_document.editor_user_policy.json}";
       description = "A DAILP editor able to perform many s3 actions";
       name = "dailp-editor-user-role";
     };
