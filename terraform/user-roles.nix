a934155730c9c8b1f0c56ea8c80efc22313bfbd3
Naomi Trevino <trevino.n@northeastern.edu>
2023-10-02 16:29:45 -0400

    refactor IAM policy and role management

--- terraform/user-roles.nix
+++ terraform/user-roles.nix
@@ -6,8 +6,24 @@ let
  word-annotations = "word-annotations-list/*";
  user-audio = "user-uploaded-audio/*";
 in {
-  # Policy Definitions
+  # Policy Documents
   config.data.aws_iam_policy_document = {
+    trusted_assume_role_policy.statement = {
+      effect = "Allow";
+      principals = {
+        type = "Federated";
+        identifiers = ["cognito-identity.amazonaws.com"];
+      };
+      actions = "sts:AssumeRoleWithWebIdentity";
+      condition = {
+        test = "StringEquals";
+        variable = "cognito-identity.amazonaws.com:aud";
+        values = [
+          "us-east-1:6d544733-83e2-4d38-baa3-195d3bfdf54b"
+        ];
+      };
+    };
+
     basic_user_policy.statement = {
       sid = "dailp-basic-user-policy";
       effect = "Allow";
@@ -39,8 +55,6 @@ in {
         ];
       };
     };
-
-    # TODO slim down this role's access
     editor_user_policy.statement = {
         sid = "dailp-editor-user-policy";
         actions = [
@@ -56,24 +70,57 @@ in {
       };
     };
 
-  # Role Definitions
-  config.resource.aws_iam_role = {
-    basic_user_role = {
-      assume_role_policy = "$\{data.aws_iam_policy_document.basic_user_policy.json}";
-      description = "A basic DAILP user with read only s3 access";
-      name = "dailp-user";
+  config.resource = {
+    # Policy Resource Definitions
+    aws_iam_policy = {
+      dailp_basic_user_policy = {
+        policy = "$\{data.aws_iam_policy_document.basic_user_policy.json}";
+        name = "dailp-basic-user-policy";
+        description = "Allows minimal S3 read permissions for unauthenticated users.";
+      };
+      dailp_editor_policy = {
+        policy = "$\{data.aws_iam_policy_document.editor_user_policy.json}";
+        name = "dailp-editor-policy";
+        description = "A policy giving editors permission to read and write to DAILPs S3 media bucket";
+      };
+      dailp_user_contributor_policy = {
+        policy = "$\{data.aws_iam_policy_document.contributor_user_policy.json}";
+        name = "dailp-user-contributor-policy";
+        description = "Permissions for DAILP users at the Contributor level.";
+      };
     };
-
-    contributor_user_role = {
-      assume_role_policy = "$\{data.aws_iam_policy_document.contributor_user_policy.json}";
-      description = "A DAILP contributor with limited read and write s3 access";
-      name = "dailp-user-contributor";
+    # Role Resource Definitions
+    aws_iam_role = {
+      dailp_user = {
+        assume_role_policy = "$\{data.aws_iam_policy_document.trusted_assume_role_policy.json}";
+        description = "A basic unauthenticated DAILP user";
+        name = "dailp-user";
+      };
+      dailp_user_contributor = {
+        assume_role_policy = "$\{data.aws_iam_policy_document.trusted_assume_role_policy.json}";
+        description = "A DAILP user with basic read/update permissions.";
+        name = "dailp-user-contributor";
+      };
+      dailp_user_editor = {
+        assume_role_policy = "$\{data.aws_iam_policy_document.trusted_assume_role_policy.json}";
+        description = "A user with editorial permissions on DAILP";
+        name = "dailp-user-editor";
+      };
     };
-
-    editor_user_role = {
-      assume_role_policy = "$\{data.aws_iam_policy_document.editor_user_policy.json}";
-      description = "A DAILP editor able to perform many s3 actions";
-      name = "dailp-user-editor";
+    # Bind policies to roles
+    aws_iam_role_policy_attachment = {
+      basic_user = {
+        role = "$\{aws_iam_role.dailp_user.name}";
+        policy_arn = "$\{aws_iam_policy.dailp_basic_user_policy.arn}";
+      };
+      editor = {
+        role = "$\{aws_iam_role.dailp_user_editor.name}";
+        policy_arn = "$\{aws_iam_policy.dailp_editor_policy.arn}";
+      };
+      contributor = {
+        role = "$\{aws_iam_role.dailp_user_contributor.name}";
+        policy_arn = "$\{aws_iam_policy.dailp_user_contributor_policy.arn}";
+      };
     };
   };
 }
