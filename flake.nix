242178165edcd2047aae2cdab5a6bcff3ec6a1c3
Naomi Trevino <trevino.n@northeastern.edu>
2023-10-02 20:49:59 -0400

    Fix broken references

--- flake.nix
+++ flake.nix
@@ -112,7 +112,7 @@
         tfInit = ''
           cp -f ${terraformConfig}/config.tf.json ./
           export AWS_ACCESS_KEY_ID=${getEnv "AWS_ACCESS_KEY_ID"}
-          export AWS_ACCESS_KEY_ID=${getEnv "AWS_SECRET_ACCESS_KEY"}
+          export AWS_SECRET_ACCESS_KEY=${getEnv "AWS_SECRET_ACCESS_KEY"}
           export TF_DATA_DIR=$(pwd)/.terraform
           ${tf} init -upgrade
         '';
