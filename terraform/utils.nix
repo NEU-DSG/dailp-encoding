60c7e7a1a0872a04d1804df55c10353d70f1b156
Naomi Trevino <trevino.n@northeastern.edu>
2023-10-18 21:12:11 -0400

    add user group domain control to TF

--- terraform/utils.nix
+++ terraform/utils.nix
@@ -1,2 +1,2 @@
-# For now, this module is a higher order function generating a function that creates
+# For now, this module is a higher order function generating a function that creates a template string from a given string
 stage: base: "dailp-${stage}-${base}"
