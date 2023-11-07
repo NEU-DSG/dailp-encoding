acaaa8a607e601d467f9d85df13d43ca9a3386ac
Naomi Trevino <trevino.n@northeastern.edu>
2023-11-07 14:21:58 -0500

    do not manage tf state bucket and table in uat

--- terraform/bootstrap.nix
+++ terraform/bootstrap.nix
@@ -18,43 +18,49 @@ with lib; {
   config = {
     # This S3 bucket holds the Terraform state file. There should be a separate
     # bucket and table for each stage, like development and production.
-    resource.aws_s3_bucket.tf_state_bucket = {
-      # Generate a unique bucket name with the given prefix.
-      bucket = config.setup.state.bucket;
-      acl = "private";
-      server_side_encryption_configuration.rule.apply_server_side_encryption_by_default =
+    ${ # UAT does not manage the dev state bucket
+      if config.setup.stage != "uat" then {
+      resource.aws_s3_bucket.tf_state_bucket = {
+        # Generate a unique bucket name with the given prefix.
+        bucket = config.setup.state.bucket;
+        acl = "private";
+        server_side_encryption_configuration.rule.apply_server_side_encryption_by_default =
         {
           sse_algorithm = "AES256";
         };
-      versioning.enabled = true;
-      tags = { 
-        "Name" = "Terraform state storage";
-        "Terraform" = "true";
-        # "nu:function" = "storage>nu:application dailp-app";
-      } // config.setup.global_tags;
-      lifecycle.prevent_destroy = true;
-      logging = {
-        target_bucket = config.setup.access_log_bucket;
-        target_prefix = "/${config.setup.state.bucket}";
+        versioning.enabled = true;
+        tags = { 
+          "Name" = "Terraform state storage";
+          "Terraform" = "true";
+          # "nu:function" = "storage>nu:application dailp-app";
+        } // config.setup.global_tags;
+        lifecycle.prevent_destroy = true;
+        logging = {
+          target_bucket = config.setup.access_log_bucket;
+          target_prefix = "/${config.setup.state.bucket}";
+        };
       };
-    };
 
-    resource.aws_dynamodb_table.tf_lock_state = {
-      name = config.setup.state.table;
-      billing_mode = "PAY_PER_REQUEST";
-      # Hash key is required, and must be an attribute.
-      hash_key = "LockID";
-      # Attribute LockID is required for Terraform to use this table for lock state.
-      attribute = {
-        name = "LockID";
-        type = "S";
+      resource.aws_dynamodb_table.tf_lock_state = {
+        name = config.setup.state.table;
+        billing_mode = "PAY_PER_REQUEST";
+        # Hash key is required, and must be an attribute.
+        hash_key = "LockID";
+        # Attribute LockID is required for Terraform to use this table for lock state.
+        attribute = {
+          name = "LockID";
+          type = "S";
+        };
+        tags = {
+          Name = config.setup.state.table;
+          BuiltBy = "Terraform";
+        } // config.setup.global_tags;
+        lifecycle.prevent_destroy = true;
       };
-      tags = {
-        Name = config.setup.state.table;
-        BuiltBy = "Terraform";
-      } // config.setup.global_tags;
-      lifecycle.prevent_destroy = true;
-    };
+    } 
+    else {}
+    }
+    
 
     # Use the bucket and table provisioned above to fuel Terraform state
     # management. This is a bit of a chicken and egg situation, where the
