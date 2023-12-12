29ad69fd28ea451c5bf6b62b484b70b15490197f
nole2701 <nole2701@gmail.com>
2023-12-12 23:52:10 +0700

    changed the return type for the UpdateParagraph mutation using the existing paragrah_by_id function

--- website/src/graphql/dailp/index.ts
+++ website/src/graphql/dailp/index.ts
@@ -655,7 +655,7 @@ export type Mutation = {
   readonly updateDocumentMetadata: Scalars["UUID"]
   readonly updatePage: Scalars["Boolean"]
   /** Mutation for paragraph and translation editing */
-  readonly updateParagraph: Scalars["UUID"]
+  readonly updateParagraph: DocumentParagraph
   readonly updateWord: AnnotatedForm
 }
 
@@ -1976,9 +1976,11 @@ export type UpdateParagraphMutationVariables = Exact<{
   paragraph: ParagraphUpdate
 }>
 
-export type UpdateParagraphMutation = {
-  readonly __typename?: "Mutation"
-} & Pick<Mutation, "updateParagraph">
+export type UpdateParagraphMutation = { readonly __typename?: "Mutation" } & {
+  readonly updateParagraph: {
+    readonly __typename?: "DocumentParagraph"
+  } & Pick<DocumentParagraph, "id" | "translation" | "index">
+}
 
 export type UpdateContributorAttributionMutationVariables = Exact<{
   contribution: UpdateContributorAttribution
@@ -2717,7 +2719,11 @@ export function useRemoveBookmarkMutation() {
 }
 export const UpdateParagraphDocument = gql`
   mutation UpdateParagraph($paragraph: ParagraphUpdate!) {
-    updateParagraph(paragraph: $paragraph)
+    updateParagraph(paragraph: $paragraph) {
+      id
+      translation
+      index
+    }
   }
 `
 
