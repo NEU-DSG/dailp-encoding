2cdd0fe66e6c6af48e7ea59d95de2c4cd1329470
Naomi Trevino <trevino.n@northeastern.edu>
2023-12-29 16:20:13 -0500

    add auth controls to CWKW

--- website/src/pages/cwkw/cwkw-layout.tsx
+++ website/src/pages/cwkw/cwkw-layout.tsx
@@ -11,6 +11,7 @@ import { useRouteParams } from "src/renderer/PageShell"
 import { collectionRoute } from "src/routes"
 import { colors, mediaQueries } from "src/style/constants"
 import "src/style/global.css"
+import { LoginHeaderButton } from "../auth/user-auth-layout"
 import { useDialog } from "../edited-collections/edited-collection-context"
 import CWKWBanner from "./assets/cwkw-banner.svg"
 import MobileCWKWBanner from "./assets/mobile-cwkw-banner.svg"
@@ -60,6 +61,7 @@ const CWKWLayout: React.FC = ({ children }) => {
               </Link>
             </h1>
           </div>
+          <LoginHeaderButton className={css.loginHeader} />
           <HeaderPrefDrawer color={colors.body} />
         </div>
       </header>
