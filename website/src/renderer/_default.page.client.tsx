a7ac9988910e5ddc4023e18e23ae2256d3dd5765
Charlie McVicker <charlie@mcvickernet.com>
2024-01-16 14:58:36 -0500

    refactor(gql): auth exchange decides which gql endpoint to use

--- website/src/renderer/_default.page.client.tsx
+++ website/src/renderer/_default.page.client.tsx
@@ -1,22 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { Client } from "urql"
-import { getCredentials } from "src/auth"
 import { clientSideGraphqlClient, clientSsrExchange } from "src/graphql"
 import { PageContext, PageShell, rootElementId } from "./PageShell"
-
-async function getClient() {
-  const token = await getCredentials()
-  return clientSideGraphqlClient(token)
-}
-let clientPromise: null | Promise<Client> = null
 
 export async function render(pageContext: PageContext) {
   const { urqlState } = pageContext
   clientSsrExchange.restoreData(urqlState)
-  const client = clientPromise
-    ? await clientPromise
-    : await (clientPromise = getClient())
+  const client = clientSideGraphqlClient()
   const page = <PageShell pageContext={pageContext} client={client} />
   const elem = document.getElementById(rootElementId)
   ReactDOM.hydrate(page, elem)
