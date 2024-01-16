41a7c322da53a2902906060e9136e52df87892ca
Charlie McVicker <charlie@mcvickernet.com>
2024-01-16 14:01:27 -0500

    clean up names and move all createClient calls to one file

--- website/src/client/layout.tsx
+++ website/src/client/layout.tsx
@@ -1,11 +1,11 @@
 import React, { useMemo } from "react"
 import { Provider as GraphQLProvider } from "urql"
+import { clientSideGraphqlClient } from "src/graphql"
 import { useCredentials } from "../auth"
-import { graphqlClient } from "../graphql/client"
 
 export const LayoutClient = (p: { children: any }) => {
   const token = useCredentials() // uses credentials
-  const client = useMemo(() => graphqlClient(token), [token])
+  const client = useMemo(() => clientSideGraphqlClient(token), [token])
   if (token) {
     return <GraphQLProvider value={client}>{p.children}</GraphQLProvider>
   } else {
