d29066fb92ecf720e237fff0927880939ec7dde7
Charlie McVicker <charlie@mcvickernet.com>
2024-01-16 14:58:36 -0500

    refactor(gql): auth exchange decides which gql endpoint to use

--- website/src/client/layout.tsx
+++ website/src/client/layout.tsx
@@ -1,14 +1,6 @@
-import React, { useMemo } from "react"
-import { Provider as GraphQLProvider } from "urql"
-import { clientSideGraphqlClient } from "src/graphql"
-import { useCredentials } from "../auth"
+import React from "react"
 
+// TODO: delete this component
 export const LayoutClient = (p: { children: any }) => {
-  const token = useCredentials() // uses credentials
-  const client = useMemo(() => clientSideGraphqlClient(token), [token])
-  if (token) {
-    return <GraphQLProvider value={client}>{p.children}</GraphQLProvider>
-  } else {
-    return p.children
-  }
+  return p.children
 }
