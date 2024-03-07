da1359c09fa845761d65faca010b2cb6fe79a2f0
Naomi Trevino <trevino.n@northeastern.edu>
2024-03-07 14:51:30 -0500

    fix time format

--- terraform/database-sql.nix
+++ terraform/database-sql.nix
@@ -44,7 +44,7 @@ in {
       vpc_security_group_ids = config.servers.database.security_group_ids;
       final_snapshot_identifier = "${name}-primary-final-snapshot";
       backup_retention_period = 14;
-      backup_window = "7:00-7:59";
+      backup_window = "07:00-07:59";
 
       # Server times are in UTC, so this is 12am-3am PT
       apply_immediately = true;
