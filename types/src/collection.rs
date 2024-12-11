d899f91a1146fdb1d007ac8bb540ff90ba8b7a70
Naomi Trevino <naomi.e.trevino@gmail.com>
2024-12-11 12:01:38 -0800

    run `cargo clippy --fix`

--- types/src/collection.rs
+++ types/src/collection.rs
@@ -82,14 +82,14 @@ impl CollectionChapter {
     }
 
     async fn slug(&self) -> String {
-        slugify((&self.path.last()).unwrap())
+        slugify(self.path.last().unwrap())
     }
 
     async fn document(&self, context: &Context<'_>) -> FieldResult<Option<AnnotatedDoc>> {
         if let Some(doc_id) = &self.document_id {
             Ok(context
                 .data::<DataLoader<Database>>()?
-                .load_one(doc_id.clone())
+                .load_one(*doc_id)
                 .await?)
         } else {
             Ok(None)
