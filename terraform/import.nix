9a6c0c01218b83e5196b6759394b823d9cd74a4a
Naomi Trevino <naomi.e.trevino@gmail.com>
2025-04-16 14:20:01 -0700

    import bastion aws ec2 instance

--- terraform/import.nix
+++ terraform/import.nix
@@ -11,5 +11,9 @@ in {
       to = "module.bastion_host.aws_iam_role_policy.main[0]";
       id = "${prefixName "bastion"}:${prefixName "bastion"}";
     }
+    {
+      to = "module.bastion_host.aws_instance.default";
+      id = "i-0e4166c8cb6363bb3"; # TODO update value later. 
+    }
   ];
 }
