27dee6ea2fc917e74fc9a3afd42566f598faa676
nole2701 <nole2701@gmail.com>
2025-09-05 21:40:50 +0700

    commented out the code that blocks console logs for now

--- website/src/renderer/_default.page.server.tsx
+++ website/src/renderer/_default.page.server.tsx
@@ -133,8 +133,8 @@ const baseScript = escapeInject`
       var global = window;
     }
     var process = ${clientProcess};
-    if (${skipConsoleLog}) {
-      console.log = function(){};
-    }
   </script>
 `
+// if (${skipConsoleLog}) {
+//       console.log = function(){};
+//     }
