171dbbaa425b8dc8f1c7cf17949dc176dac9f359
Dennis Wang <66754085+denniwang@users.noreply.github.com>
2025-10-14 07:51:57 -0700

    Admin menu editing backend (#501)
    * started edit nav page
    
    * drag drop works smoothly
    
    * add / delete items, confirm on delete
    
    * crud works
    
    * confirm delete + reset button
    
    * real menu uses dailp query now
    
    * dev check
    
    * ui smooth, gql req still breaking
    
    * subitems get moved too
    
    * no more slug as an input
    
    * removed cid and mobile nav
    
    * cleanup, also fixed slug saving
    
    * brought back client ids because hard to type previously
    
    * cleanup
    
    * added missing migration
    
    * fixed menu-by-slug query
    
    * dev-check
    
    * sqlx prepare
    
    * Revert frontend changes
    
    This reverts commit 0a52894d8e24f463b2aaf8c7a29079ff6ad251f9.
    
    Revert "started edit nav page"
    
    This reverts commit 5b152d9242b08a751df2fa52598cf23b61184553.
    
    Revert "drag drop works smoothly"
    
    This reverts commit cb34c0fd6a5d898cd3a7092c5a6af5b14c4a9638.
    
    Revert "add / delete items, confirm on delete"
    
    This reverts commit adfa916228843e023b0803f80570906c559f9a4e.
    
    old edit nav menu and disabled clg
    
    Revert "confirm delete + reset button"
    
    This reverts commit d46dd5492e37fa530858dc4a893f694008a238bc.
    
    Revert "real menu uses dailp query now"
    
    This reverts commit 8e80a14a99d70c3f061f76f4643da157dc93ae31.
    
    reverted  frontend edit nav menu page
    
    Revert "ui smooth, gql req still breaking"
    
    This reverts commit 7effc1f6694705d736a57bc7f7067819e90cf7c9.
    
    Revert "removed cid and mobile nav"
    
    This reverts commit 649b029d9456d911057d766539c9eb29b0ef637f.
    
    Revert "removed cid and mobile nav"
    
    This reverts commit 649b029d9456d911057d766539c9eb29b0ef637f.
    
    Revert "cleanup, also fixed slug saving"
    
    This reverts commit 403b5f4e6df1f0d780edade5f9cf0bf13a4bf0a7.
    
    Revert "brought back client ids because hard to type previously"
    
    This reverts commit 0ffe442b4c76e8fed6583ee1a1504fdf9b3549aa.
    
    Revert "cleanup"
    
    This reverts commit d25dd71b9d7059bc5295bd198f7da0f8d1bff3f9.
    
    * added insert menu query , menu migration in rust now
    
    might lead to some 404s because i removed trailing slashes from all menu paths
    
    * formatting
    
    * sqlx prepare

--- migration/src/main.rs
+++ migration/src/main.rs
@@ -6,6 +6,7 @@ mod contributors;
 mod early_vocab;
 mod edited_collection;
 mod lexical;
+mod menu;
 mod spreadsheets;
 mod tags;
 mod translations;
@@ -31,8 +32,8 @@ async fn main() -> Result<()> {
     println!("Migrating Image Sources...");
     migrate_image_sources(&db).await?;
 
-    //println!("Migrating contributors...");
-    //contributors::migrate_all(&db).await?;
+    println!("Migrating contributors...");
+    contributors::migrate_all(&db).await?;
 
     println!("Migrating tags to database...");
     tags::migrate_tags(&db).await?;
@@ -45,8 +46,11 @@ async fn main() -> Result<()> {
 
     migrate_data(&db).await?;
 
-    // println!("Migrating connections...");
-    // connections::migrate_connections(&db).await?;
+    println!("Migrating connections...");
+    connections::migrate_connections(&db).await?;
+
+    println!("Migrating menu...");
+    menu::migrate_menu(&db).await?;
 
     println!("Migrating collections...");
     edited_collection::migrate_edited_collection(&db).await?;
