a23012040aa9f1d68723cc83a4d23597c013e3e4
Dennis Wang <66754085+denniwang@users.noreply.github.com>
2025-10-15 12:33:25 -0400

    emptying edit menu page

--- website/src/pages/admin/edit-menu.page.tsx
+++ website/src/pages/admin/edit-menu.page.tsx
@@ -1,25 +1,27 @@
-import React from "react"
-import { UserRole } from "src/auth"
-import { AuthGuard } from "src/components/auth-guard"
-import { EditableNavMenu } from "src/components/editable/EditNavMenu"
-import Layout from "src/layout"
+//import React from "react"
+//import { UserRole } from "src/auth"
+//import { AuthGuard } from "src/components/auth-guard"
+//import { EditableNavMenu } from "src/components/editable/EditNavMenu"
+//import Layout from "src/layout"
 
-const EditMenuPage = () => {
-  // dennis todo : change required role to admin
-  // dennis todo: fetch current menu id from db or whatever so we can edit that version
+//const EditMenuPage = () => {
+  //// dennis todo : change required role to admin
+  //// dennis todo: fetch current menu id from db or whatever so we can edit that version
 
-  return (
-    <>
-      <AuthGuard requiredRole={UserRole.Editor}>
-        <Layout>
-          <main>
-            <h1> Edit menu page</h1>
-            <EditableNavMenu navMenuSlug="default-nav" />
-          </main>
-        </Layout>
-      </AuthGuard>
-    </>
-  )
-}
+  //return (
+    //<>
+      //<AuthGuard requiredRole={UserRole.Editor}>
+        //<Layout>
+          //<main>
+            //<h1> Edit menu page</h1>
+            //<EditableNavMenu navMenuSlug="default-nav" />
+          //</main>
+        //</Layout>
+      //</AuthGuard>
+    //</>
+  //)
+//}
 
-export const Page = EditMenuPage
+//export const Page = EditMenuPage
+
+export {}
