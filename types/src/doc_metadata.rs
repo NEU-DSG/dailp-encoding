de49c7bfad6b2f35eead03b9f8598e0deafa65a4
Alison Ye <alisony755@gmail.com>
2025-11-05 14:23:07 -0500

    Add approval status

--- types/src/doc_metadata.rs
+++ types/src/doc_metadata.rs
@@ -1,14 +1,15 @@
 /// Document metadata
-use crate::{document::DocumentReference, ContributorReference};
-
-use async_graphql::{Enum, SimpleObject};
+use async_graphql::{Enum, SimpleObject, ComplexObject};
 use serde::{Deserialize, Serialize};
+use sqlx::{postgres::PgValueRef, Decode, FromRow, Postgres};
 use std::collections::HashMap;
 use uuid::Uuid;
 
+use crate::ContributorReference;
+
 /// Represents the status of a suggestion made by a contributor
-#[derive(Deserialize, Serialize, Enum, Clone, Copy, PartialEq, Eq)]
-pub enum Status {
+#[derive(Serialize, Deserialize, Enum, Debug, Clone, Copy, PartialEq, Eq)]
+pub enum ApprovalStatus {
     /// Suggestion is still waiting for or undergoing review
     Pending,
     /// Suggestion has been approved
@@ -17,79 +18,110 @@ pub enum Status {
     Rejected,
 }
 
-/// Record to store a subject heading that reflects Indigenous knowledge
-/// practices associated with a document
-#[derive(Clone, SimpleObject)]
-pub struct SubjectHeading {
-    /// UUID for the subject heading
-    pub id: Uuid,
-    /// Documents associated with the subject heading
-    pub documents: Vec<DocumentReference>,
-    /// Name of the subject heading
-    pub name: String,
-    /// Status (pending, approved, rejected) of a subject heading
-    pub status: Status,
+/// Allows SQLx to convert Postgres "approval_status" enum values into the corresponding Rust "ApprovalStatus"
+impl<'r> Decode<'r, Postgres> for ApprovalStatus {
+    fn decode(value: PgValueRef<'r>) -> Result<Self, Box<dyn std::error::Error + Send + Sync>> {
+        let s: &str = <&str as Decode<'r, Postgres>>::decode(value)?;
+        match s {
+            "pending" => Ok(Self::Pending),
+            "approved" => Ok(Self::Approved),
+            "rejected" => Ok(Self::Rejected),
+            _ => Err(format!("invalid approval status: {}", s).into()),
+        }
+    }
 }
 
-/// Stores the physical or digital medium associated with a document
-#[derive(Clone, SimpleObject)]
-pub struct Format {
-    /// UUID for the format
-    pub id: Uuid,
-    /// Documents associated with the format
-    pub documents: Vec<DocumentReference>,
-    /// Name of the format
-    pub name: String,
+/// Converts a string value ("approved", "pending", or "rejected") into an ApprovalStatus enum variant
+impl TryFrom<String> for ApprovalStatus {
+    type Error = anyhow::Error;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match value.as_str() {
+            "approved" => Ok(ApprovalStatus::Approved),
+            "pending" => Ok(ApprovalStatus::Pending),
+            "rejected" => Ok(ApprovalStatus::Rejected),
+            _ => Err(anyhow::anyhow!("Invalid approval status: {}", value)),
+        }
+    }
 }
 
 /// Stores the genre associated with a document
-#[derive(Clone, SimpleObject)]
+#[derive(Clone, Debug, Serialize, Deserialize, FromRow)]
+#[graphql(complex)]
 pub struct Genre {
     /// UUID for the genre
     pub id: Uuid,
-    /// Documents associated with the genre
-    pub documents: Vec<DocumentReference>,
     /// Name of the genre
     pub name: String,
+    /// Status (pending, approved, rejected) of a genre
+    pub status: ApprovalStatus,
+}
+
+/// Stores the physical or digital medium associated with a document
+#[derive(Clone, Debug, Serialize, Deserialize, FromRow)]
+#[graphql(complex)]
+pub struct Format {
+    /// UUID for the format
+    pub id: Uuid,
+    /// Name of the format
+    pub name: String,
+    /// Status (pending, approved, rejected) of a format
+    pub status: ApprovalStatus,
+}
+/// Record to store a keyword associated with a document
+#[derive(Clone, Debug, Serialize, Deserialize, FromRow)]
+#[graphql(complex)]
+pub struct Keyword {
+    /// UUID for the keyword
+    pub id: Uuid,
+    /// Name of the keyword
+    pub name: String,
+    /// Status (pending, approved, rejected) of a keyword
+    pub status: ApprovalStatus,
+}
+
+/// Record to store a subject heading that reflects Indigenous knowledge
+/// practices associated with a document
+#[derive(Clone, Debug, Serialize, Deserialize, FromRow)]
+#[graphql(complex)]
+pub struct SubjectHeading {
+    /// UUID for the subject heading
+    pub id: Uuid,
+    /// Name of the subject heading
+    pub name: String,
+    /// Status (pending, approved, rejected) of a subject heading
+    pub status: ApprovalStatus,
 }
 
 /// Stores a language associated with a document
-#[derive(Clone, SimpleObject)]
+#[derive(Clone, Debug, Serialize, Deserialize, FromRow)]
+#[graphql(complex)]
 pub struct Language {
     /// UUID for the language
     pub id: Uuid,
-    /*
-    Tag for the language within the DAILP system
-    Could be useful for managing similar language names or extending this to
-    adding tags for language, dialect, and script combinations later on
-    */
-    pub dailp_tag: String,
-    /// Documents associated with the language
-    pub documents: Vec<DocumentReference>,
     /// Name of the language
     pub name: String,
+    // Name a language uses for itself
+    pub autonym: Option<String>,
+    /// Status (pending, approved, rejected) of a language
+    pub status: ApprovalStatus,
 }
 
 /// Stores a spatial coverage associated with a document
-#[derive(Clone, SimpleObject)]
+#[derive(Clone, Debug, Serialize, Deserialize, FromRow)]
+#[graphql(complex)]
 pub struct SpatialCoverage {
     /// UUID for the place
     pub id: Uuid,
-    /*
-    Tag for the spatial coverage within the DAILP system
-    Could be useful for managing places with similar names or places
-    with multiple names
-    */
-    pub dailpTag: String,
-    /// Documents associated with the spatial coverage
-    pub documents: Vec<DocumentReference>,
     /// Name of the place
     pub name: String,
+    /// Status (pending, approved, rejected) of a spatial coverage
+    pub status: ApprovalStatus,
 }
 
 /// Stores citation information for a document
 /// TODO: Add more fields to cover a variety of format types
-#[derive(Clone, Serialize, Deserialize, SimpleObject)]
+#[derive(Clone, Serialize, Deserialize)]
 pub struct Citation {
     /// UUID for the citation
     pub id: Uuid,
