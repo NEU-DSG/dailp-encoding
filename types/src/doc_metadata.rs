4ff48a07f3c9c9764490bdac760a2180941941bc
Alison Ye <alisony755@gmail.com>
2025-11-05 14:54:05 -0500

    Overwrite file with version from main

--- types/src/doc_metadata.rs
+++ types/src/doc_metadata.rs
@@ -3,46 +3,18 @@ use crate::{document::DocumentReference, ContributorReference};
 
 use async_graphql::{Enum, SimpleObject};
 use serde::{Deserialize, Serialize};
-use sqlx::{postgres::PgValueRef, Decode, Postgres};
 use std::collections::HashMap;
 use uuid::Uuid;
 
 /// Represents the status of a suggestion made by a contributor
-#[derive(Serialize, Deserialize, Enum, Debug, Clone, Copy, PartialEq, Eq)]
-pub enum ApprovalStatus {
+#[derive(Deserialize, Serialize, Enum, Clone, Copy, PartialEq, Eq)]
+pub enum Status {
     /// Suggestion is still waiting for or undergoing review
     Pending,
     /// Suggestion has been approved
     Approved,
     /// Suggestion has been rejected
     Rejected,
-}
-
-/// Allows SQLx to convert Postgres "approval_status" enum values into the corresponding Rust "ApprovalStatus"
-impl<'r> Decode<'r, Postgres> for ApprovalStatus {
-    fn decode(value: PgValueRef<'r>) -> Result<Self, Box<dyn std::error::Error + Send + Sync>> {
-        let s: &str = <&str as Decode<'r, Postgres>>::decode(value)?;
-        match s {
-            "pending" => Ok(Self::Pending),
-            "approved" => Ok(Self::Approved),
-            "rejected" => Ok(Self::Rejected),
-            _ => Err(format!("invalid approval status: {}", s).into()),
-        }
-    }
-}
-
-/// Converts a string value ("approved", "pending", or "rejected") into an ApprovalStatus enum variant
-impl TryFrom<String> for ApprovalStatus {
-    type Error = anyhow::Error;
-
-    fn try_from(value: String) -> Result<Self, Self::Error> {
-        match value.as_str() {
-            "approved" => Ok(ApprovalStatus::Approved),
-            "pending" => Ok(ApprovalStatus::Pending),
-            "rejected" => Ok(ApprovalStatus::Rejected),
-            _ => Err(anyhow::anyhow!("Invalid approval status: {}", value)),
-        }
-    }
 }
 
 /// Record to store a subject heading that reflects Indigenous knowledge
@@ -56,7 +28,7 @@ pub struct SubjectHeading {
     /// Name of the subject heading
     pub name: String,
     /// Status (pending, approved, rejected) of a subject heading
-    pub status: ApprovalStatus,
+    pub status: Status,
 }
 
 /// Stores the physical or digital medium associated with a document
