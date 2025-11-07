43de7a336eba86b64b461e2e6b5265b83e0de25e
Alison Ye <alisony755@gmail.com>
2025-11-07 11:14:50 -0500

    Don't cast k.status as ApprovalStatus

--- types/queries/many_keywords_for_documents.sql
+++ types/queries/many_keywords_for_documents.sql
@@ -3,7 +3,7 @@ select
     dk.document_id,
     k.id,
     k.name,
-    k.status::approval_status as status
+    k.status
 from keyword k
 join document_keyword dk on k.id = dk.keyword_id
 where dk.document_id = any($1);
