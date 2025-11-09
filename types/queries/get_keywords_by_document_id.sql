0d3d314e9c6dfe1177fabad0833c189fd1e2b046
Alison Ye <alisony755@gmail.com>
2025-11-08 19:03:09 -0500

    Cast status as text in SQL queries?

--- types/queries/get_keywords_by_document_id.sql
+++ types/queries/get_keywords_by_document_id.sql
@@ -1,7 +1,7 @@
 select 
-    id, 
-    name, 
-    coalesce(status, 'pending') as status
+    k.id, 
+    k.name, 
+    k.status::text as status
 from keyword k
 join document_keyword dk on dk.keyword_id = k.id
 where dk.document_id = $1;
