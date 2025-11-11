771a9a08bedc70fe23621da34c9d1737d3ec7552
Alison Ye <alisony755@gmail.com>
2025-11-11 09:44:38 -0500

    Add missing languages_ids field to DocumentMetadata initializations

--- graphql/src/query.rs
+++ graphql/src/query.rs
@@ -770,6 +770,7 @@ impl Mutation {
             sources: vec![source],
             collection: None,
             genre: None,
+            languages_ids: None,
             contributors: vec![contributor],
             translation: None,
             page_images: None,
