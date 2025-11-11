16d83d9acd2cf3c84ba72b22b6975d5a261e63e0
Alison Ye <alisony755@gmail.com>
2025-11-10 22:11:57 -0500

    Explicitly cast status as ApprovalStatus in subject heading queries

--- types/queries/many_subject_headings_for_documents.sql
+++ types/queries/many_subject_headings_for_documents.sql
@@ -1,9 +1,8 @@
 -- Fetch all subject headings linked to a given set of documents
 select
-    dsh.document_id,
     sh.id,
     sh.name,
-    status::approval_status as status
+    status as "status: ApprovalStatus"
 from subject_heading sh
 join document_subject_heading dsh on sh.id = dsh.subject_heading_id
 where dsh.document_id = any($1);
