40824a28d85c0df3404ae56432728bcd4d31d20c
Alison Ye <alisony755@gmail.com>
2025-11-11 15:17:19 -0500

    Add missing format_id to DocumentMetadata initializations

--- graphql/src/query.rs
+++ graphql/src/query.rs
@@ -770,6 +770,7 @@ impl Mutation {
             sources: vec![source],
             collection: None,
             genre: None,
+            format_id: None,
             contributors: vec![contributor],
             translation: None,
             page_images: None,
