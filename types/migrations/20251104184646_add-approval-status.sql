478d00b1982288813bc3a198c0bdaecc2b595a8e
Alison Ye <alisony755@gmail.com>
2025-11-11 17:11:34 -0500

    Change timestamp of approval status migration

--- types/migrations/20251104184646_add-approval-status.sql
+++
@@ -1,7 +0,0 @@
--- Shared enum for approval status of contributor suggestions
-drop type if exists approval_status cascade;
-create type approval_status as enum (
-  'pending', 
-  'approved', 
-  'rejected'
-);
