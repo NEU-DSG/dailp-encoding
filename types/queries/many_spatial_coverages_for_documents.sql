db983162b1920c4e53879f2a032e5824cee382c2
Alison Ye <alisony755@gmail.com>
2025-11-11 12:40:14 -0500

    Explicitly cast status as ApprovalStatus in spatial coverage queries

--- types/queries/many_spatial_coverages_for_documents.sql
+++ types/queries/many_spatial_coverages_for_documents.sql
@@ -1,9 +1,8 @@
 -- Fetch all spatial coverages linked to a given set of documents
 select
-    dsc.document_id,
     sc.id,
     sc.name,
-    sc.status::approval_status as status
+    sc.status as "status: ApprovalStatus"
 from spatial_coverage sc
 join document_spatial_coverage dsc on sc.id = dsc.spatial_coverage_id
 where dsc.document_id = any($1);
