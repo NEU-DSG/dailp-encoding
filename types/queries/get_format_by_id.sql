3c3b53db92d7725e126b1b875a0095ad42b18266
Alison Ye <alisony755@gmail.com>
2025-11-11 15:06:57 -0500

    Explicitly cast status as ApprovalStatus

--- types/queries/get_format_by_id.sql
+++ types/queries/get_format_by_id.sql
@@ -1,6 +1,6 @@
 select 
-    id, 
-    name, 
-    status::approval_status as status
-from doc_format
+    f.id, 
+    f.name, 
+    f.status as "status: ApprovalStatus"
+from doc_format f
 where id = $1;
