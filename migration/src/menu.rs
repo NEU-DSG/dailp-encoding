eb35a56df97f0c852f331f8b909c1256a872fd79
Dennis Wang <66754085+denniwang@users.noreply.github.com>
2025-11-13 12:37:06 -0500

    Revert "Admin Page Editing (#509)"
    This reverts commit 50430e140e54b95eaf12c57fef9fb48768c68be4.

--- migration/src/menu.rs
+++ migration/src/menu.rs
@@ -9,37 +9,37 @@ pub async fn migrate_menu(db: &Database) -> anyhow::Result<()> {
     slug: "default-nav".to_string(),
     items: serde_json::from_str(
 "[
+  {\"label\":\"Home\",\"path\":\"/\",\"children\":[]},
+  {\"label\":\"Tools\",\"path\":\"/tools\",\"children\":[
   {\"label\":\"Glossary of Terms\",\"path\":\"/glossary\"},
   {\"label\":\"Word Search\",\"path\":\"/search\"},
-  {\"label\":\"Home\",                \"path\":\"/\",\"children\":[]},
-  {\"label\":\"Tools\",               \"path\":\"/tools\",\"children\":[
-  {\"label\":\"Further Learning\",    \"path\":\"/tools/further-learning\"}
+  {\"label\":\"Further Learning\",\"path\":\"/credit\"}
   ]},
-  {\"label\":\"About\",               \"path\":\"/\",\"children\":[
-  {\"label\":\"Support\",             \"path\":\"/about/support\"},
-  {\"label\":\"Goals\",               \"path\":\"/about/goals\"},
-  {\"label\":\"Team\",                \"path\":\"/about/team\"},
-  {\"label\":\"Former Contributors\", \"path\":\"/about/former-contributors\"},
-  {\"label\":\"Project History\",     \"path\":\"/about/project-history\"},
-  {\"label\":\"Why DAILP? Why Now?\", \"path\":\"/about/why-this-archive-why-now\"},
-  {\"label\":\"References\",          \"path\":\"/about/sources\"}
+  {\"label\":\"About\",\"path\":\"/\",\"children\":[
+  {\"label\":\"Support\",\"path\":\"/support\"},
+  {\"label\":\"Goals\",\"path\":\"/home/goals\"},
+  {\"label\":\"Team\",\"path\":\"/home/credits\"},
+  {\"label\":\"Former Contributors\",\"path\":\"/former-contributors\"},
+  {\"label\":\"Project History\",\"path\":\"/home/project-history\"},
+  {\"label\":\"Why DAILP? Why Now?\",\"path\":\"/home/why-this-archive-why-now\"},
+  {\"label\":\"References\",\"path\":\"/sources\"}
   ]},
-  {\"label\":\"Stories\",                                           \"path\":\"/\",\"children\":[
-  {\"label\":\"Dollie Duncan Letter Inspires Opera\",               \"path\":\"/story/dollie-duncan-letter-inspires-opera\"},
-  {\"label\":\"About the Editors of CWKW\",                         \"path\":\"/story/editors-of-cwkw\"},
-  {\"label\":\"The Importance of Language Persistence\",            \"path\":\"/story/the-importance-of-language-persistence\"},
-  {\"label\":\"UKB Advisory Board Members & Translators Discover Family Voices in DAILP’s Work\",\"path\":\"/story/ukb-advisory-board-members-translators-discover-family-voices-in-dailps-work\"},
-  {\"label\":\"DAILP honored with grant from the Henry R. Luce Foundation\",\"path\":\"/story/dailp-honored-with-grant-from-the-henry-r-luce-foundation\"}
+  {\"label\":\"Stories\",\"path\":\"/stories\",\"children\":[
+  {\"label\":\"Dollie Duncan Letter Inspires Opera\",\"path\":\"/dollie-duncan-letter-inspires-opera\"},
+  {\"label\":\"About the Editors of CWKW\",\"path\":\"/editors\"},
+  {\"label\":\"The Importance of Language Persistence\",\"path\":\"/dailp-language-preservation-narrative\"},
+  {\"label\":\"UKB Advisory Board Members & Translators Discover Family Voices in DAILP’s Work\",\"path\":\"/ukb-advisory-board-members-translators-discover-family-voices-in-dailps-work\"},
+  {\"label\":\"DAILP honored with grant from the Henry R. Luce Foundation\",\"path\":\"/stories/the-luce-foundation/dailp-honored-with-grant-from-the-henry-r-luce-foundation\"}
   ]},
-  {\"label\":\"Spotlights\",                              \"path\":\"/\",\"children\":[
-  {\"label\":\"DAILP Spotlight: Melissa Torres\",         \"path\":\"/spotlight/melissa-torres\"},
-  {\"label\":\"DAILP Spotlight: Hazelyn Aroian\",         \"path\":\"/spotlight/hazelyn-aroian\"},
-  {\"label\":\"DAILP Spotlight: Meg Cassidy\",            \"path\":\"/spotlight/meg-cassidy\"},
-  {\"label\":\"DAILP Spotlight: Milan Evans\",            \"path\":\"/spotlight/milan-evans\"},
-  {\"label\":\"Persisting in Cherokee with Mary Rae\",    \"path\":\"/spotlight/persisting-in-cherokee-with-mary-rae\"}
-
+  {\"label\":\"Spotlights\",\"path\":\"/category/uncategorized\",\"children\":[
+  {\"label\":\"DAILP Spotlight: Melissa Torres\",\"path\":\"/stories/melissa-torres\"},
+  {\"label\":\"DAILP Spotlight: Hazelyn Aroian\",\"path\":\"/dailp-spotlight-hazelyn-aroian\"},
+  {\"label\":\"DAILP Spotlight: Meg Cassidy\",\"path\":\"/dailp-spotlight-meg-cassidy\"},
+  {\"label\":\"DAILP Spotlight: Milan Evans\",\"path\":\"/dailp-spotlight-milan-evans\"},
+  {\"label\":\"Persisting in Cherokee with Mary Rae\",\"path\":\"/persisting-in-cherokee-with-mary-%ea%ae%8a%ea%ae%85-rae\"}
   ]}
 ]").unwrap()};
+
     db.insert_menu(menu).await?;
     Ok(())
 }
