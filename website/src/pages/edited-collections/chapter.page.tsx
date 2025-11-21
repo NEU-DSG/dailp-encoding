4a977ba086f84bbdd58b2f45630f8180c2f32af0
Dennis Wang <66754085+denniwang@users.noreply.github.com>
2025-11-20 22:31:47 -0800

    Page editing frontendv3 (#530)
    * insert page and page by slug queries backend
    
    * create page draft
    
    * fixing insert page function
    
    * editor and page-by-name
    
    * using path instead of page now
    
    * migration
    
    * changed to use upsert
    
    * page contents uses db, edit page
    
    * renamed wordpress pages to dailp page
    
    * pulled menu queries
    
    * dailp page checks if current route is in menu table
    
    * removed double layout
    
    * fixed menu + page routes, edit page button
    
    * dev check
    
    * fixed page edit link + migration csv path
    
    * dev check
    
    * glossary page, search page
    
    * clean up edit page flow
    
    * Revert "Admin Page Editing (#509)"
    
    This reverts commit 50430e140e54b95eaf12c57fef9fb48768c68be4.
    
    * force new pages to route to pages
    
    * added collection pages
    
    * added glossary and search page to cwkw
    
    * Delete page-by-name.page.tsx
    
    * dev check
    
    * remove wordpress routes, installed react markdown
    
    * react markdown works
    
    * cleanup
    
    * check html
    
    * dev check
    
    ---------
    
    Co-authored-by: Naomi Trevino <n.trevino@northeastern.edu>

--- website/src/pages/edited-collections/chapter.page.tsx
+++ website/src/pages/edited-collections/chapter.page.tsx
@@ -6,6 +6,7 @@ import { chapterRoute, collectionRoute } from "src/routes"
 import * as util from "src/style/utils.css"
 import CWKWLayout from "../cwkw/cwkw-layout"
 import * as css from "../cwkw/cwkw-layout.css"
+import { DailpPageContents } from "../dailp.page"
 import { DocumentTitleHeader, TabSet } from "../documents/document.page"
 import * as chapterStyle from "./chapter.css"
 import { useDialog, useSubchapters } from "./edited-collection-context"
@@ -64,7 +65,13 @@ const ChapterPage = (props: {
                     )}
                 </Breadcrumbs>
               </header>
-              <WordpressPage slug={`/${chapter.slug.replace(/_/g, "-")}`} />
+              {/* dennis TODO: replace with dailp stuff after migration is done with these pages */}
+              <DailpPageContents
+                path={`/${props.collectionSlug}/${chapter.slug.replace(
+                  /_/g,
+                  "-"
+                )}`}
+              />
             </>
           ) : null}
 
