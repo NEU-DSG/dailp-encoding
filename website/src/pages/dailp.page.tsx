da7fa273cb8810c3c4ac545508ef94b8b4c771c2
Naomi Trevino <naomi.e.trevino@gmail.com>
2025-12-05 16:00:18 -0800

    render placeholder content on dailp pages

--- website/src/pages/dailp.page.tsx
+++ website/src/pages/dailp.page.tsx
@@ -23,86 +23,87 @@ const DailpPage = (props: DailpPageProps) => (
 
 export const Page = DailpPage
 
-// export const DailpPageContents = (props: { path: string }) => {
-//   const [{ data, fetching }] = usePageByPathQuery({
-//     variables: { path: props.path },
-//   })
+export const DailpPageContents = (props: { path: string }) => {
+  return <p>DAILP Page placeholder</p>
+  //   const [{ data, fetching }] = usePageByPathQuery({
+  //     variables: { path: props.path },
+  //   })
 
-//   const [{ data: menuData }] = useMenuBySlugQuery({
-//     variables: { slug: "default-nav" },
-//     pause: fetching, // Don't fetch menu until page query is complete
-//   })
+  //   const [{ data: menuData }] = useMenuBySlugQuery({
+  //     variables: { slug: "default-nav" },
+  //     pause: fetching, // Don't fetch menu until page query is complete
+  //   })
 
-//   const page = data?.pageByPath
-//   const menu = menuData?.menuBySlug
-//   const firstBlock = page?.body?.[0]
-//   const content =
-//     firstBlock?.__typename === "Markdown" ? firstBlock.content : null
+  //   const page = data?.pageByPath
+  //   const menu = menuData?.menuBySlug
+  //   const firstBlock = page?.body?.[0]
+  //   const content =
+  //     firstBlock?.__typename === "Markdown" ? firstBlock.content : null
 
-//   const isInMenu = (slug: string) => {
-//     return (
-//       menu?.items
-//         ?.flatMap((item) => item.items)
-//         .some((item) => item?.path === slug) ?? false
-//     )
-//   }
+  //   const isInMenu = (slug: string) => {
+  //     return (
+  //       menu?.items
+  //         ?.flatMap((item) => item.items)
+  //         .some((item) => item?.path === slug) ?? false
+  //     )
+  //   }
 
-//   if (fetching) {
-//     return <p>Loading...</p>
-//   }
+  //   if (fetching) {
+  //     return <p>Loading...</p>
+  //   }
 
-//   if (!page || !content) {
-//     return <p>Page content not found. {props.path}</p>
-//   }
-//   let collectionSlugs = ["cwkw", "willie-jumper-stories"]
+  //   if (!page || !content) {
+  //     return <p>Page content not found. {props.path}</p>
+  //   }
+  //   let collectionSlugs = ["cwkw", "willie-jumper-stories"]
 
-//   // check if page belongs in a collection
-//   const collectionSlug = props.path.split("/")[1]
-//   if (collectionSlug && collectionSlugs.includes(collectionSlug)) {
-//     const userRole = useUserRole()
+  //   // check if page belongs in a collection
+  //   const collectionSlug = props.path.split("/")[1]
+  //   if (collectionSlug && collectionSlugs.includes(collectionSlug)) {
+  //     const userRole = useUserRole()
 
-//     return (
-//       <>
-//         <header>
-//           <h1>{page.title}</h1>
-//           {/* dennis todo: should be admin in the future */}
-//           {userRole === UserRole.Editor && (
-//             <Link href={`/edit${props.path}`}>Edit</Link>
-//           )}
-//         </header>
-//         {/* html chceking here please*/}
-//         {content.charAt(0) === "<" ? (
-//           <div dangerouslySetInnerHTML={{ __html: content }} />
-//         ) : (
-//           <Markdown>{content}</Markdown>
-//         )}
-//       </>
-//     )
-//   } else {
-//     // otherwise check if page is in menu
-//     if (!isInMenu(props.path)) {
-//       return (
-//         <p>Page content found. Add it to the menu to view it. {props.path}</p>
-//       )
-//     }
-//   }
+  //     return (
+  //       <>
+  //         <header>
+  //           <h1>{page.title}</h1>
+  //           {/* dennis todo: should be admin in the future */}
+  //           {userRole === UserRole.Editor && (
+  //             <Link href={`/edit${props.path}`}>Edit</Link>
+  //           )}
+  //         </header>
+  //         {/* html chceking here please*/}
+  //         {content.charAt(0) === "<" ? (
+  //           <div dangerouslySetInnerHTML={{ __html: content }} />
+  //         ) : (
+  //           <Markdown>{content}</Markdown>
+  //         )}
+  //       </>
+  //     )
+  //   } else {
+  //     // otherwise check if page is in menu
+  //     if (!isInMenu(props.path)) {
+  //       return (
+  //         <p>Page content found. Add it to the menu to view it. {props.path}</p>
+  //       )
+  //     }
+  //   }
 
-//   const userRole = useUserRole()
+  //   const userRole = useUserRole()
 
-//   return (
-//     <>
-//       <header>
-//         <h1>{page.title}</h1>
-//         {/* dennis todo: should be admin in the future */}
-//         {userRole === UserRole.Editor && (
-//           <Link href={`/edit${props.path}`}>Edit</Link>
-//         )}
-//       </header>
-//       {content.charAt(0) === "<" ? (
-//         <div dangerouslySetInnerHTML={{ __html: content }} />
-//       ) : (
-//         <Markdown>{content}</Markdown>
-//       )}
-//     </>
-//   )
-// }
+  //   return (
+  //     <>
+  //       <header>
+  //         <h1>{page.title}</h1>
+  //         {/* dennis todo: should be admin in the future */}
+  //         {userRole === UserRole.Editor && (
+  //           <Link href={`/edit${props.path}`}>Edit</Link>
+  //         )}
+  //       </header>
+  //       {content.charAt(0) === "<" ? (
+  //         <div dangerouslySetInnerHTML={{ __html: content }} />
+  //       ) : (
+  //         <Markdown>{content}</Markdown>
+  //       )}
+  //     </>
+  //   )
+}
