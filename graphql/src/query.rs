e2ee332d9cc5d1edf8eb755c40800cc8c582eeb3
Katie Nguyen <nguyen.katie.business@gmail.com>
2026-02-10 13:13:07 -0500

    Implemented list_users

--- graphql/src/query.rs
+++ graphql/src/query.rs
@@ -378,6 +378,15 @@ impl Query {
             .await?)
     }
 
+    /// Gets all dailp_user with their id, username, and role for now
+    async fn list_users(&self, context: $Context<'_>) -> FieldResult<Vec<User>> {
+        Ok(context
+            .data::<DataLoader<Database>>()?
+            .loader()
+            .all_users()
+            .await?)
+    }
+
     async fn abbreviation_id_from_short_name(
         &self,
         context: &Context<'_>,
@@ -637,6 +646,8 @@ impl Mutation {
             .await?
             .ok_or_else(|| anyhow::format_err!("Failed to load document"))?)
     }
+
+
 
     /// Decide if a piece of word audio should be included in edited collection
     #[graphql(guard = "GroupGuard::new(UserGroup::Editors)")]
