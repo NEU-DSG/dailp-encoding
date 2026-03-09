774f6d60e25c70ef91f9ae195e42fd63c7398726
Katie Nguyen <nguyen.katie.business@gmail.com>
2026-03-09 08:23:04 -0400

    added role dropdown

--- website/src/components/user-management/user-list.tsx
+++ website/src/components/user-management/user-list.tsx
@@ -1,14 +1,27 @@
 import React from "react"
 import { Helmet } from "react-helmet"
-import { useAllUsersQuery } from "src/graphql/dailp"
+import { UserGroup, useAllUsersQuery } from "src/graphql/dailp"
 import { boldWordRow, wordRow } from "../../pages/timeline.css"
+
+const userRoles = [
+  UserGroup.Administrators,
+  UserGroup.Editors,
+  UserGroup.Contributors,
+  UserGroup.Readers,
+] as const
 
 export const UserList = () => {
   const [{ data, fetching, error }] = useAllUsersQuery()
+  const handleRoleChange = (userId: string, newRole: string) => {
+    // katie todo: connect to aws after rust update
+  }
+
+  const handleRemoveUser = (userId: string, displayName: string) => {
+    // katie todo: connect to aws after rust update
+  }
 
   return (
     <>
-      <Helmet title="Users" />
       <main>
         {fetching ? (
           <>Loading...</>
@@ -21,7 +34,25 @@ export const UserList = () => {
             {data.listUsers.map((user) => (
               <div key={user.id} className={wordRow}>
                 <div>{user.displayName}</div>
-                <div>Role: {user.role}</div>
+                <div>
+                  Role:{" "}
+                  <select
+                    value={user.role || UserGroup.Readers}
+                    onChange={(e) =>
+                      handleRoleChange(user.id, e.target.value as UserGroup)
+                    }
+                  >
+                    {userRoles.map((item) => (
+                      <option key={item} value={item}>
+                        {item}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <div>{/*pending invitation function here*/}</div>
+                <div>
+                  <button>Remove User</button>
+                </div>
               </div>
             ))}
           </div>
