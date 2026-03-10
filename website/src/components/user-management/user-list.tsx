ea32a0c73b5fed89765b5d903ef40f60bdac61c8
Katie Nguyen <nguyen.katie.business@gmail.com>
2026-03-10 16:22:11 -0400

    changing user role feature implemented

--- website/src/components/user-management/user-list.tsx
+++ website/src/components/user-management/user-list.tsx
@@ -1,18 +1,49 @@
 import React from "react"
-import { AllUsersQuery, UserGroup, useAllUsersQuery } from "src/graphql/dailp"
+import {
+  AllUsersQuery,
+  User,
+  UserGroup,
+  useAllUsersQuery,
+  useUpdateUserMutation,
+} from "src/graphql/dailp"
 import * as css from "./user-list.css"
-
-const userRoles = [
-  { value: UserGroup.Readers, label: "Reader" },
-  { value: UserGroup.Contributors, label: "Contributor" },
-  { value: UserGroup.Editors, label: "Editor" },
-  { value: UserGroup.Administrators, label: "Admin" },
-]
 
 export const UserList = () => {
   const [{ data, fetching, error }] = useAllUsersQuery()
-  const handleRoleChange = (userId: string, newRole: string) => {
-    // katie todo: connect to aws after rust update
+  const [updateUserResult, updateUser] = useUpdateUserMutation()
+
+  const userRoles = [
+    { value: UserGroup.Readers, label: "Reader" },
+    { value: UserGroup.Contributors, label: "Contributor" },
+    { value: UserGroup.Editors, label: "Editor" },
+    { value: UserGroup.Administrators, label: "Admin" },
+  ]
+
+  const handleRoleChange = (
+    user: AllUsersQuery["listUsers"][number],
+    newRole: UserGroup
+  ) => {
+    const confirm = window.confirm(
+      "Press confirm to change user's role to " + newRole
+    )
+
+    if (!newRole) return
+
+    if (!confirm) {
+      return
+    }
+
+    const userUpdate = {
+      id: user.id,
+      displayName: user.displayName || "",
+      role: newRole,
+      avatarUrl: null,
+      bio: null,
+      location: null,
+      organization: null,
+    }
+
+    updateUser({ user: userUpdate })
   }
 
   const handleRemoveUser = (userId: string, displayName: string) => {
@@ -27,7 +58,7 @@ export const UserList = () => {
   const getPendingStatus = (
     user: AllUsersQuery["listUsers"][number]
   ): JSX.Element => {
-    const isPending = true
+    const isPending = user
 
     if (isPending) {
       return <span style={{ color: "#999" }}>(Pending)</span>
@@ -52,10 +83,17 @@ export const UserList = () => {
                   Role:{" "}
                   <select
                     value={user.role || UserGroup.Readers}
-                    onChange={(e) => handleRoleChange(user.id, e.target.value)}
+                    onChange={(e) => {
+                      {
+                        const role = userRoles.find(
+                          (r) => r.value === e.target.value
+                        )?.value
+                        if (role) handleRoleChange(user, role)
+                      }
+                    }}
                   >
                     {userRoles.map((item) => (
-                      <option key={item.value} value={item.value}>
+                      <option key={item.label} value={item.value}>
                         {item.label}
                       </option>
                     ))}
