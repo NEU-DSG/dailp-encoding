214cb231245b87205330bb516a46b1b7dea4dba5
Naomi Trevino <n.trevino@northeastern.edu>
2026-03-20 14:38:38 -0400

    format all page paths to exclude trailing / (#691)
    Co-authored-by: Naomi Trevino <naomi.e.trevino@gmail.com>

--- website/src/renderer/_error.page.tsx
+++ website/src/renderer/_error.page.tsx
@@ -19,11 +19,13 @@ export const Page = () => {
 
 const ClientPage = () => {
   const location = useLocation()
+  const path = location.pathname.endsWith("/")
+    ? location.pathname.slice(0, -1)
+    : location.pathname
+
   const [{ data, fetching }] = Dailp.usePageByPathQuery({
     variables: {
-      path: location.pathname.endsWith("/")
-        ? location.pathname.slice(0, -1)
-        : location.pathname,
+      path: path,
     },
     requestPolicy: "network-only",
   })
@@ -31,7 +33,7 @@ const ClientPage = () => {
   if (fetching) {
     return null
   } else if (data && data.pageByPath) {
-    return <DailpPageContents path={location.pathname} />
+    return <DailpPageContents path={path} />
   } else {
     return <NotFound />
   }
