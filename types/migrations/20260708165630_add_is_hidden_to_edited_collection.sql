c9a558c2d3648d083e5414e5b0af85784e995c73
SonAndrea <sonandrea12@gmail.com>
2026-07-09 14:45:13 -0400

    default hidden is now true

--- types/migrations/20260708165630_add_is_hidden_to_edited_collection.sql
+++ types/migrations/20260708165630_add_is_hidden_to_edited_collection.sql
@@ -1,2 +1,2 @@
--- Adding is_hidden column to 
-alter table edited_collection add column is_hidden boolean not null default false;
+-- Adding is_hidden column to collections defaulted as hidden
+alter table edited_collection add column is_hidden boolean not null default true;
